c3f57817df2448b6b12018f322f0403134a39f21
Steven Silvester <steven.silvester@ieee.org>
2015-03-29 21:30:40 -0500

    Add the Transform script utils and a stub for a test

---
+++ metatlas/tests/test_transform.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+import os
+import sys
+
+import tables
+
+from metatlas.trns_transform_mzML_LCMS_to_MetaboliteAtlas2_RunSet import transform
+
+
+def test_transform():
+    pass
