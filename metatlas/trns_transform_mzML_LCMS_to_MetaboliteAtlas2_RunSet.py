d127f44dba1342d09bbfa76a33f8a9832c103fc6
Steven Silvester <steven.silvester@ieee.org>
2015-03-29 21:31:30 -0500

    More implementation of the transform service"

--- metatlas/trns_transform_mzML_LCMS_to_MetaboliteAtlas2_RunSet.py
+++ metatlas/trns_transform_mzML_LCMS_to_MetaboliteAtlas2_RunSet.py
@@ -4,7 +4,7 @@
 import os
 import sys
 import logging
-import hashlib
+import datetime
 
 # 3rd party imports
 import simplejson
@@ -13,10 +13,15 @@ import simplejson
 try:
     import biokbase.Transform.script_utils as script_utils
 except ImportError:
-    script_utils = None
+    from . import script_utils
+
+# Metatlas Imports
+from metatlas import mzml_loader
+
 
 if sys.version.startswith('3'):
     unicode = str
+
 
 # transformation method that can be called if this module is imported
 # Note the logger has different levels it could be run.
@@ -28,7 +33,7 @@ if sys.version.startswith('3'):
 def transform(shock_service_url=None, handle_service_url=None,
               output_file_name=None, input_directory=None,
               working_directory=None, shock_id=None, handle_id=None,
-              input_mapping=None, fasta_reference_only=False,
+              input_mapping=None,
               level=logging.INFO, logger=None):
     """
     Converts mzML file to MetaboliteAtlas2_RunSet json string.
@@ -42,12 +47,12 @@ def transform(shock_service_url=None, handle_service_url=None,
                           If the output file name is not specified the name
                           will default
                           to the name of the input file appended with
-                           '_runset'
+                           '_finfo
         input_directory: The directory where files will be read from.
         working_directory: The directory the resulting json file will be
                            written to.
-        shock_id: Shock id for the fasta file if it already exists in shock
-        handle_id: Handle id for the fasta file if it already exists as a
+        shock_id: Shock id for the hdf file if it already exists in shock
+        handle_id: Handle id for the hdf file if it already exists as a
                     handle
         input_mapping: JSON string mapping of input files to expected types.
                        If you don't get this you need to scan the input
@@ -55,7 +60,7 @@ def transform(shock_service_url=None, handle_service_url=None,
         level: Logging level, defaults to logging.INFO.
 
     Returns:
-        JSON file on disk that can be saved as a KBase workspace object.
+        JSON files on disk that can be saved as a KBase workspace objects.
 
     Author:
         Steven Silvester
@@ -67,79 +72,53 @@ def transform(shock_service_url=None, handle_service_url=None,
     logger.info("Starting conversion of mzML to MetaboliteAtlas2.RunSet")
     token = os.environ.get('KB_AUTH_TOKEN')
 
-    if input_mapping is None:
-        logger.info("Scanning for mzML files.")
-
-        valid_extensions = [".mzML"]
-
-        files = os.listdir(input_directory)
-        mzml_files = [x for x in files
-                             if os.path.splitext(x)[-1] in valid_extensions]
-
-        assert len(mzml_files) != 0
-
-        logger.info("Found {0}".format(str(mzml_files)))
-
-        input_file_name = os.path.join(input_directory, files[0])
-
-        if len(mzml_files) > 1:
-            logger.warning("Not sure how to handle multiple mzML files in this"
-                           " context. Using {0}".format(input_file_name))
-    else:
-        join = os.path.join
-        input_file_name = join(join(input_directory, "mzML.LCMS"),
-                               simplejson.loads(input_mapping)["mzML.LCMS"])
-
-    logger.info("Building Object.")
-
-    if not os.path.isfile(input_file_name):
-        raise Exception("The input file name {0} is not a file!"
-                        .format(input_file_name))
-
     if not os.path.isdir(args.working_directory):
         raise Exception("The working directory {0} is not a valid directory!"
                         .format(working_directory))
 
-    fasta_filesize = os.stat(input_file_name).st_size
-    if fasta_filesize > 1000000000:
-        # Fasta file too large to save sequences into the ContigSet object.
-        contigset_warn = """The FASTA input file seems to be too large. A ContigSet
-                            object will be created without sequences, but will
-                            contain a reference to the file."""
-        logger.warning(contigset_warn)
+    logger.info("Scanning for mzML files.")
 
-    # TODO: parse the input file here
-    run_dict = dict()
-    run_dict["md5"] = hashlib.md5()
-    run_dict["id"] = output_file_name
-    run_dict["name"] = output_file_name
-    run_dict["source"] = "KBase"
-    run_dict["source_id"] = os.path.basename(input_file_name)
+    valid_extensions = [".mzML"]
 
-    if output_file_name is None:
-        # default to input file name minus file extenstion adding "_run"
-        # to the end
-        base = os.path.basename(input_file_name)
-        output_file_name = "{0}_run.json".format(os.path.splitext(base)[0])
+    files = os.listdir(input_directory)
+    mzml_files = [x for x in files
+                         if os.path.splitext(x)[-1] in valid_extensions]
 
-    if shock_id is None:
+    assert len(mzml_files) != 0
+
+    logger.info("Found {0} files".format(len(mzml_files)))
+
+    for fname in files:
+        path = os.path.join(input_directory, fname)
+
+        if not os.path.isfile(path):
+            raise Exception("The input file name {0} is not a file!"
+                            .format(path))
+
+        hdf_file = mzml_loader(path)
+
         shock_info = script_utils.upload_file_to_shock(logger,
-                shock_service_url, input_file_name, token=token)
-        shock_id = shock_info["id"]
+                shock_service_url, hdf_file, token=token)
 
-    run_dict["Run_data_ref"] = shock_id
+        run_info = dict()
+        run_info['name'] = fname.replace('.mzML', '')
+        run_info['polarity'] = 0
+        run_info['group'] = ''
+        run_info['inclusion_order'] = ''
+        run_info['normalization_factor'] = ''
+        run_info['retention_correction'] = ''
+        run_info['creator'] = ''
+        run_info['creation_date'] = datetime.datetime.utcnow()
+        run_info["data"] = shock_info["id"]
 
-    # For future development if the type is updated to the handle_reference
-    # instead of a shock_reference
+        output_file_name = fname.replace('.mzML', '_finfo')
 
-    # This generates the json for the object
-    objectString = simplejson.dumps(run_dict, sort_keys=True, indent=4)
+        # This generates the json for the object
+        objectString = simplejson.dumps(run_info, sort_keys=True, indent=4)
 
-    logger.info("Run data structure creation completed.  Writing out JSON.")
-
-    output_file_path = os.path.join(working_directory, output_file_name)
-    with open(output_file_path, "w") as outFile:
-        outFile.write(objectString)
+        output_file_path = os.path.join(working_directory, output_file_name)
+        with open(output_file_path, "w") as outFile:
+            outFile.write(objectString)
 
     logger.info("Conversion completed.")
 
