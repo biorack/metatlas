2b0d8720939ba233656045ae7274afa33f4c5d8d
Steven Silvester <steven.silvester@ieee.org>
2015-04-13 22:34:34 -0500

    Update the test

--- metatlas/tests/test_loader.py
+++ metatlas/tests/test_loader.py
@@ -16,5 +16,5 @@ def test_loader():
     assert table.nrows == 933367
     assert table[0][0] == 59.01387023925781
     assert table[-1][0] == 1666.9520263671875
-    scan_time = [y['scan_time'] for y in table.where('(ms_level==1)')]
+    scan_time = [y['rt'] for y in table.where('(ms_level==1)')]
     assert len(scan_time) == 933367
