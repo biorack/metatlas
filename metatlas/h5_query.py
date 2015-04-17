7996696349c8a9163838c628b8438aee0e2b8bf3
Steven Silvester <steven.silvester@ieee.org>
2015-04-16 19:42:14 -0500

    Invert heatmap plot and adjust formatter

--- metatlas/h5_query.py
+++ metatlas/h5_query.py
@@ -76,10 +76,13 @@ def plot_heatmap(arr, rt_bins, mz_bins, title='HeatMap for Sample', **kwargs):
     kwargs.setdefault('interpolation', 'nearest')
     kwargs.setdefault('aspect', 'auto')
     kwargs.setdefault('cmap', 'YlGnBu_r')
+    kwargs.setdefault('origin', 'lower')
 
     kwargs['extent'] = [rt_bins[0], rt_bins[-1], mz_bins[0], mz_bins[-1]]
 
     plt.imshow(arr, **kwargs)
+    plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
+
     plt.xlabel('Time (min)')
     plt.ylabel('Mass (m/z)')
     plt.title(title)
