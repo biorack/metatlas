6a4bdde530cec58d6838fae4e0789991d72d2563
Steven Silvester <steven.silvester@ieee.org>
2015-04-16 20:33:54 -0500

    Call the plot commands and add a debug print

--- metatlas/tests/test_query.py
+++ metatlas/tests/test_query.py
@@ -6,7 +6,8 @@ import tables
 
 from metatlas.mzml_loader import mzml_to_hdf, get_test_data
 from metatlas.h5_query import (
-    get_XIC, get_data, get_spectrogram, get_HeatMapRTMZ)
+    get_XIC, get_data, get_spectrogram, get_HeatMapRTMZ, 
+    plot_heatmap, plot_spectrogram, plot_xic)
 
 fid = None
 
@@ -33,6 +34,7 @@ def test_xicof():
     xicof_scidb = np.load(os.path.join(dname, 'xic_scidb.npy'))
 
     assert rmse(y, xicof_scidb[:, 1]) < 0.06
+    plot_xic(x, y)
 
 
 def test_spectrogram():
@@ -41,6 +43,8 @@ def test_spectrogram():
     assert np.allclose(x.mean(), 855.718857765)
     assert y.min() >= 0
     assert y.max() <= 100
+
+    plot_spectrogram(x, y)
 
 
 def test_heatmap():
@@ -52,10 +56,12 @@ def test_heatmap():
 
     data = get_HeatMapRTMZ(fid, 1000, 1000, 1, 0, min_mz=50)
     assert np.allclose(data['mz_bins'][0], 50.8247537537)
+    plot_heatmap(data['arr'], data['rt_bins'], data['mz_bins'])
 
 
 def test_get_data():
     data = get_data(fid, 1, 0, min_rt=5, min_mz=100, precursor_MZ=0)
     assert np.allclose(data['i'].mean(), 7825.55387233)
     assert np.allclose(data['mz'][0], 100.979026794)
-    assert np.allclose(data['rt'][-1], 5.00666666031)
+    assert np.allclose(data['rt'][-1], 5.00666666031), data['rt'][-1]
+
