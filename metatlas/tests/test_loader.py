b83bd0563c2fae7d2e50b6891449a8488964a8ea
Steven Silvester <steven.silvester@ieee.org>
2015-04-18 21:25:27 -0500

    Factor out the loader check

--- metatlas/tests/test_loader.py
+++ metatlas/tests/test_loader.py
@@ -13,6 +13,10 @@ def test_loader():
 
     mzml_to_hdf(path, out_file_name=out_file)
 
+    check_file(out_file)
+
+
+def check_file(out_file):
     fid = tables.open_file(out_file)
     table = fid.root.spectra
 
@@ -31,12 +35,4 @@ def test_loader_main():
                 '--output', out_file,
                 '--debug']
     main()
-
-    fid = tables.open_file(out_file)
-    table = fid.root.spectra
-
-    assert table.nrows == 933367
-    assert table[0][0] == 59.01387023925781
-    assert table[-1][0] == 1666.9520263671875
-    scan_time = [y['rt'] for y in table.where('(ms_level==1)')]
-    assert len(scan_time) == 933367
+    check_file(out_file)
