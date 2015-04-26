6eaaa432cf137af69d32d667a9a5d23198f206e1
Steven Silvester <steven.silvester@ieee.org>
2015-04-26 16:50:11 -0500

    Expose functions at the package level

--- metatlas/__init__.py
+++ metatlas/__init__.py
@@ -1,2 +1,4 @@
 __version__ = '0.2'
 from .mzml_loader import mzml_to_hdf
+from .h5_query import plot_heatmap, plot_spectrogram, plot_xic
+from .h5_query import get_data, get_XIC, get_HeatMapRTMZ, get_spectrogram
