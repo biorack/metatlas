afe155c705be73985b24537bb18f3e528764b061
Steven Silvester <steven.silvester@ieee.org>
2015-06-14 16:25:40 -0500

    Add xcms to the local narrative install

--- narrative-install.sh
+++ narrative-install.sh
@@ -1,11 +1,13 @@
 set -x
+R --version
+sudo Rscript xcms-install.R
 
 # Install a local version of the Kbase narrative
 conda remove -n kbase --all -y
-conda create -y -n kbase python=2.7 scipy pytables matplotlib readline
+conda create -y -n kbase python=2.7 scipy pytables matplotlib readline pandas
 source activate kbase
 pip install -e .
-pip install pexpect
+pip install pexpect rpy2
 
 # based on instructions in narrative/docs/developer.md
 rm -rf bootstrap
@@ -21,5 +23,5 @@ git submodule update
 export VIRTUAL_ENV="kbase"
 bash install.sh
 
-# point to the CI services      
+# point to the CI services
 sed -i '' 's:kbase.us/services/:ci.kbase.us/services/:' src/config.json
