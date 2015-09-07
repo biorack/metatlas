6b43a0f07ccb1da7585e99ddd02abaaceb2110f3
Steven Silvester <steven.silvester@ieee.org>
2015-09-07 13:25:07 -0500

    Bump the version

--- metatlas/__init__.py
+++ metatlas/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2'
+__version__ = '0.3'
 from .mzml_loader import mzml_to_hdf
 from .h5_query import plot_heatmap, plot_spectrogram, plot_XIC
 from .h5_query import get_data, get_XIC, get_heatmap, get_spectrogram
