b254f809f700df0a152e1b67572eed044d6a5be5
Jimmy E. Touma <aitatanit@gmail.com>
2016-02-29 22:49:26 -0600

    added show_atlases to gui.py

--- metatlas/gui.py
+++ metatlas/gui.py
@@ -54,6 +54,76 @@ def create_qgrid(objects, options=None):
     return grid
 
 
+def show_atlases(username=None):
+    import pandas as pd
+    mz_header = ['compound', 'adduct', 'polarity', 'mz', 'mz_tolerance', 'mz_tolerance_units', 'uname']
+    mz_dict = dict()
+
+    rt_header = ['compound', 'lcms_run', 'rt_max', 'rt_min', 'rt_peak', 'rt_units', 'uname']
+    rt_dict = dict()
+
+    grid = create_qgrid([])
+
+    atlases_select = widgets.Select(description = 'Atlases:', options = [])
+    search_string = widgets.Text(description = "Atlas Name")
+    mz_button = widgets.Button(description = 'mz')
+    rt_button = widgets.Button(description = 'rt')
+
+    def atlas_list (sender):
+        wild_card = search_string.value
+        atlas = retrieve('Atlas',name= wild_card, username='*')
+        atlases_list = list()
+        for cc in atlas:
+            atlases_list.append(cc.name)
+
+        atlases_select.options = atlases_list
+
+    def get_mz(b):
+        atlas = retrieve('Atlas', name = atlases_select.value, username='*')
+
+        #- clear the dictionary
+        for i in mz_header:
+            mz_dict[i] = list()
+
+        for x in atlas[0].compound_identifications:
+            mz_dict['compound'].append(str(x.compound[0].name))
+            mz_dict['adduct'].append(str(x.mz_references[0].adduct))
+            mz_dict['mz'].append(str(x.mz_references[0].mz))
+            mz_dict['polarity'].append(str(x.mz_references[0].detected_polarity))
+            mz_dict['mz_tolerance'].append(str(x.mz_references[0].mz_tolerance))
+            mz_dict['uname'].append(str(x.mz_references[0].username))
+            mz_dict['mz_tolerance_units'].append(str(x.mz_references[0].mz_tolerance_units))
+
+        grid.df = pd.DataFrame.from_dict(mz_dict)
+
+    def get_rt(b):
+        atlas = retrieve('Atlas', name = atlases_select.value, username='*')
+
+        #- clear the dictionary
+        for i in rt_header:
+            rt_dict[i] = list()
+
+        for x in atlas[0].compound_identifications:
+            rt_dict['compound'].append(str(x.compound[0].name))
+            rt_dict['lcms_run'].append(str(x.rt_references[0].lcms_run))
+            rt_dict['rt_max'].append(str(x.rt_references[0].rt_max))
+            rt_dict['rt_min'].append(str(x.rt_references[0].rt_min))
+            rt_dict['rt_peak'].append(str(x.rt_references[0].rt_peak))
+            rt_dict['uname'].append(str(x.rt_references[0].username))
+            rt_dict['rt_units'].append(str(x.rt_references[0].rt_units))
+
+        grid.df = pd.DataFrame.from_dict(rt_dict)
+
+    search_string.observe(atlas_list)
+
+    mz_button.on_click(get_mz)
+    rt_button.on_click(get_rt)
+    display(widgets.HBox((search_string, mz_button, rt_button)))
+
+    display(atlases_select)
+    display(grid)
+
+
 def show_experiments(username=None):
     """Create a gui to browse the available experiments.
     """
