4afec724c7ab049deb6472defa00c8a04311d149
Jimmy E. Touma <aitatanit@gmail.com>
2016-07-21 22:08:13 -0500

    changed plotting routine to accept named arguments

--- metatlas/helpers/mkplotsMP.py
+++ metatlas/helpers/mkplotsMP.py
@@ -1,13 +1,12 @@
 import matplotlib 
 matplotlib.use('Agg')
 import sys
-sys.path.insert(0,'/global/homes/j/jtouma/metatlas')
-from metatlas.helpers import metatlas_get_data_helper_fun as ma_data
 import yaml
 import os
 import multiprocessing as mp
 from matplotlib import pyplot as plt
 import numpy as np
+
 
 def plot_chromatogram(d,file_name, ax=None):
     import numpy as np
@@ -29,7 +28,7 @@ def plot_chromatogram(d,file_name, ax=None):
     if len(d['data']['eic']['rt']) > 1:
         x = d['data']['eic']['rt']
         y = d['data']['eic']['intensity']
-        #print len(x), len(y), len(d['data']['eic']['rt'])#, rt_min, rt_max, rt_peak
+
         ax.plot(x,y,'k-',linewidth=2.0,alpha=1.0)  
         myWhere = np.logical_and(x>=rt_min, x<=rt_max )
         ax.fill_between(x,0,y,myWhere, facecolor='c', alpha=0.3)
@@ -40,48 +39,55 @@ def plot_chromatogram(d,file_name, ax=None):
 
     ax.set_title("\n".join(wrap(file_name,54)),fontsize=12,weight='bold')
 
-    
-def plot_compounds_and_filesMP(kwargs):
-    print mp.current_process()
+
+def plot_compounds_and_files_mp(kwargs):
+    print(mp.current_process())
+
     my_data= kwargs['data'] # data for all compounds for one file
     file_name  = kwargs['file_name'] # full path of output file name
     nRows, nCols = kwargs['rowscols']
     names = kwargs['names']
-    share_y = kwargs['share_y']   
-    
+    share_y = kwargs['share_y']
+
     f,ax = plt.subplots(nRows, nCols, sharey=share_y,figsize=(8*nCols,nRows*6))
     ax = ax.flatten()
     
     for i,name in enumerate(names):
-        p = plot_chromatogram(my_data[i], name, ax=ax[i])
+        plot_chromatogram(my_data[i], name, ax=ax[i])
 
     f.savefig(file_name)    
     plt.close(f)
 
 
-def plot_compounds_and_files(info):
-    '''
-    fname is the yaml file name containing the parameters needed to perform the plots
+def plot_compounds_and_files(output_dir,
+                             data,
+                             compound_names,
+                             file_names,
+                             nCols = 8,
+                             share_y = False,
+                             processes=25,
+                             plot_types=('files', 'compounds')):
     '''
 
-            
-    nCols = info['nCols']
-    share_y = info['share_y']
-    pkl_file = info['pkl_file']
-    output_dir = info['output_dir']
-    processes = info['processes']
-    plot_types = info['plot_types']
+    Parameters
+    ----------
+    output_dir location of saved pdf plots
+    nCols number of columns per pdf file
+    share_y subplots share/not share they y axis
+    processes number of cores to use
+    plot_types compounds per file or files per compound or both
 
+    Returns
+    -------
+    nothing
+    '''
 
     # create directory if necessary
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    data = ma_data.get_dill_data(pkl_file)
-    compound_names = ma_data.get_compound_names(data)[0]
-    file_names = ma_data.get_file_names(data)
-    print 'Number of files: ', len(file_names)
-    print 'Number of compounds:', len(compound_names)
+    print('Number of files: ', len(file_names))
+    print('Number of compounds:', len(compound_names))
 
     # setup the parameters according to the request
     if 'files' in plot_types:
@@ -89,16 +95,17 @@ def plot_compounds_and_files(info):
         args_list = []
         for file_idx,my_file in enumerate(file_names):
             kwargs = {'data':data[file_idx], 
-                  'file_name': os.path.join(output_dir,my_file+'.pdf'),
-                  'rowscols':(nRows, nCols),
-                  'share_y':share_y,
-                  'names':compound_names}
+                      'file_name': os.path.join(output_dir,my_file+'.pdf'),
+                      'rowscols':(nRows, nCols),
+                      'share_y':share_y,
+                      'names':compound_names}
             args_list.append(kwargs)
 
         nprocs = min(processes, len(file_names))
         pool = mp.Pool(processes=nprocs)
-        pool.map(plot_compounds_and_filesMP, args_list)
-        print "processed ", len(args_list), " files"
+        pool.map(plot_compounds_and_files_mp, args_list)
+        print("processed ", len(args_list), " files")
+
     if 'compounds' in plot_types:
         nRows = int(np.ceil(len(file_names)/float(nCols)))
         args_list = []
@@ -108,26 +115,41 @@ def plot_compounds_and_files(info):
                 my_data.append(data[file_idx][compound_idx])
 
             kwargs = {'data':my_data,
-                  'file_name': os.path.join(output_dir, my_compound+'.pdf'),
-                  'rowscols':(nRows, nCols),
-                  'share_y':share_y,
-                  'names':file_names}
+                      'file_name': os.path.join(output_dir, my_compound+'.pdf'),
+                      'rowscols':(nRows, nCols),
+                      'share_y':share_y,
+                      'names':file_names}
             args_list.append(kwargs)
 
         nprocs = min(processes, len(compound_names))
         pool = mp.Pool(processes=nprocs)
-        pool.map(plot_compounds_and_filesMP, args_list)
-        print "processed ", len(args_list), " compounds"
+        pool.map(plot_compounds_and_files_mp, args_list)
+        print("processed ", len(args_list), " compounds")
 
 
 if __name__ == '__main__':
-    
+    sys.path.insert(0, '/global/homes/j/jtouma/metatlas')
+    from metatlas.helpers import metatlas_get_data_helper_fun as ma_data
+
     with open(sys.argv[1], 'r') as fin:
         info = yaml.load(fin)
 
-    print "\n"
-    print 50*'-'
-    print "Using ", info['processes'], " out of ", mp.cpu_count(), " available cores"
-    print 50*'-'
-    print "\n"
-    plot_compounds_and_files(info)
+    data = ma_data.get_dill_data(info['pkl_file'])
+    file_names = ma_data.get_file_names(data)
+    compound_names = ma_data.get_compound_names(data)
+
+    print("\n")
+    print(50*'-')
+    print("Using ", info['processes'], " out of ", mp.cpu_count(), " available cores")
+    print(50*'-')
+    print("\n")
+
+    plot_compounds_and_files(output_dir=info['output_dir'],
+                             data=data,
+                             compound_names=compound_names,
+                             file_names=file_names,
+                             nCols=info['nCols'],
+                             share_y=info['share_y'],
+                             processes=info['processes'],
+                             plot_types=info['plot_types'])
+
