126fc04dec830177ee63694c681c9be1d8eb9ad4
Steven Silvester <steven.silvester@ieee.org>
2016-08-21 11:48:25 -0500

    Add qgrid dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ URL = 'http://github.com/biorack/metatlas'
 LICENSE = 'MIT'
 REQUIRES = ["numpy", "pytables", "pymzml", "simplejson", "rpy2", "pandas",
             "dataset", "ipython", "traitlets", "six", "tabulate", "dill",
-            "gspread","pymysql"]
+            "gspread","pymysql", "qgrid"]
 CLASSIFIERS = """\
 Development Status :: 2 - Pre-Alpha
 Intended Audience :: Developers
@@ -70,7 +70,8 @@ if __name__ == "__main__":
         zip_safe=False,  # the package can run out of an .egg file
         install_requires=['pymzml', 'simplejson', 'requests_toolbelt',
                           'dataset', 'ipython', 'traitlets', 'six',
-                          'tabulate', 'dill', 'oauth2client', 'gspread'],
+                          'tabulate', 'dill', 'oauth2client', 'gspread',
+                          'qgrid'],
         requires=REQUIRES,
         cmdclass={'install': custom_install},
      )
