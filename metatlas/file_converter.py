c19f6512c0374498c096fcbb3eada237813549e1
Benjamin Bowen <bpbowen@lbl.gov>
2017-05-18 15:47:22 -0700

    moved parallel fun outside

--- metatlas/file_converter.py
+++ metatlas/file_converter.py
@@ -179,8 +179,8 @@ def update_metatlas(directory):
     if sys.version_info[0] < 3: 
         pool = mp.Pool(min(NPROC,len(new_files)))
         pool.map(convert, files)
-        p.close()
-        p.join()
+        pool.close()
+        pool.join()
     else:
         # #Python3
         with mp.Pool(min(NPROC,len(new_files))) as pool:
