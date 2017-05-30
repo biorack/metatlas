720db18ad214c8910e0a7038d3aacad7f3de0b5a
Matt Trinh <matt.trinh.re@gmail.com>
2017-05-30 13:08:00 -0700

    made it more PEP8

--- metatlas/integrity_monitor.py
+++ metatlas/integrity_monitor.py
@@ -25,7 +25,7 @@ from metatlas.system_utils import send_mail
 
 # TO-DO: have these vars be defined from external YML (probably nersc.yml?)
 
-# the lower the tolerance, the more relevant data you might see 
+# the lower the tolerance, the more relevant data you might see
 # at a cost of being less flexible with defined peaks
 # default is 15
 tolerance = 15
@@ -36,6 +36,8 @@ std = 229.981116596
 std_neg = 227.966564596
 
 run_times = {}
+save_path = '/project/projectdirs/metatlas/projects/istd_logs/'
+
 
 def check_metatlas():
     """
@@ -55,21 +57,24 @@ def check_metatlas():
             body += 'The invalid runs are:\n%s' % ('\n'.join(filenames))
             send_mail('Metatlas Runs are Invalid', username, body)
 
+
 def yield_ppm(dataset, name, threshold, tolerance, mz_t, rt_max):
     """
-    Helper for get_ppm. Given a name and a dataset, returns a filtered and bounded dataset for ppm under name,
-    under specific parameters.
+    Helper for get_ppm. Given a name, a dataset, intensity threshold,
+    ppm tolerance, time of highest mz, and retention time at that point,
+    returns a filtered and bounded dataset for ppm under name,
     """
     # bugs out if you divide by mz_t on the left...
     data_filter = (abs(dataset['mz'] - mz_t) * 1e6) <= tolerance * mz_t
     filtered = dataset[data_filter]
     bounded = filtered[filtered.i >= threshold]
-    bounded = bounded.loc[:,list(bounded)]
+    bounded = bounded.loc[:, list(bounded)]
     bounded[name] = (abs(bounded['mz'] - mz_t) * 1e6 / mz_t)
     if not bounded.empty and rt_max != -1:
         data_filter = abs(bounded.rt - rt_max) <= .5
         bounded = bounded[data_filter]
     return bounded
+
 
 def get_ppms(dataset, tolerance=15, mz_std=229.981116596, threshold=1e4):
     """
@@ -79,7 +84,7 @@ def get_ppms(dataset, tolerance=15, mz_std=229.981116596, threshold=1e4):
     - tolerances for what is considered to be reasonable
     - a threshold for what is defined as a peak
     """
-    # Switch off from pos data to neg data if needed
+    # # if neg file, then use std_neg: ms1_pos should be empty if so
     ms1 = 'ms1_pos'
     ms2 = 'ms2_pos'
     data = dataset[ms1]
@@ -93,24 +98,22 @@ def get_ppms(dataset, tolerance=15, mz_std=229.981116596, threshold=1e4):
     # Looking through ms1_pos
     # MS 1
     data = dataset[ms1]
-    
+
     ms1_max_i = -1
     rt_max = -1
-    
+
     # get the max i and rt at max i
     filtered = data[(abs(data['mz'] - mz_t) * 1e6) <= tolerance * mz_t]
     tmp = filtered[filtered.i >= threshold]
-    tmp = tmp.loc[:,list(tmp)]
+    tmp = tmp.loc[:, list(tmp)]
     if not tmp.empty:
         max_i_row = tmp['i'].idxmax()
         ms1_max_i = tmp.loc[max_i_row].i
         rt_max = tmp.loc[max_i_row].rt
-        
 
     d_1 = yield_ppm(data, 'ppm', threshold, tolerance, mz_t, rt_max)
 
-    # Looking through ms2_pos
-    #MS 2, pre
+    # Looking through MS2, precursor
     data = dataset[ms2]
     data_filter = (abs(data['precursor_MZ'] - mz_t) * 1e6) <= tolerance * mz_t
     filtered = data[data_filter]
@@ -119,17 +122,16 @@ def get_ppms(dataset, tolerance=15, mz_std=229.981116596, threshold=1e4):
         thresh = thresh[abs(thresh.rt - rt_max) <= .5]
 
     uniques = thresh.drop_duplicates('precursor_MZ')
-    d_2 = uniques.loc[:,list(uniques)]
+    d_2 = uniques.loc[:, list(uniques)]
     d_2['ppm'] = (abs(d_2['precursor_MZ'] - mz_t) * 1e6 / mz_t)
 
     # Double filter on mz and precursor_MZ
     # MS 2
     d_3 = yield_ppm(filtered, 'ppm', threshold, tolerance, mz_t, rt_max)
-    
-    return (d_1.reset_index(), d_2.reset_index(), d_3.reset_index(), ms1_max_i, rt_max)
 
-# if neg file, then use std_neg
-# ms1_pos should be empty
+    return (d_1.reset_index(), d_2.reset_index(), d_3.reset_index(),
+            ms1_max_i, rt_max)
+
 
 def data_verify(file_name):
     dataset = ma_data.df_container_from_metatlas_file(file_name)
@@ -143,27 +145,31 @@ def data_verify(file_name):
     samples_dict['ms2 ppm'] = ppms[2]['ppm'].tolist()
 
     samples_dict['rt'] = ppms[0]['rt'].tolist()
-    
+
     # extra stats and more data filtering
     samples_dict['ms1 ppm weighted mean'] = weighted_mean(ppms[0])
-    samples_dict['ms2_precursor ppm weighted mean'] = weighted_mean(ppms[1]) #hacky
+    # hacky
+    samples_dict['ms2_precursor ppm weighted mean'] = weighted_mean(ppms[1])
     samples_dict['ms2 ppm weighted mean'] = weighted_mean(ppms[2])
-    
+
     samples_dict['ms1 max intensity'] = ppms[3]
     samples_dict['rt at max intensity'] = ppms[4]
     samples_dict['acquisition timestamp'] = run_times[u'/global%s' % file_name]
-    samples_dict['acquisition time'] = datetime.fromtimestamp(samples_dict['acquisition timestamp']).strftime('%Y-%m-%d %H:%M:%S')
-    
-        
+    samples_dict['acquisition time'] = datetime.fromtimestamp(
+        samples_dict['acquisition timestamp']).strftime('%Y-%m-%d %H:%M:%S')
+
     for key in samples_dict.keys():
         if samples_dict[key] == [] or samples_dict[key] == -1:
             samples_dict[key] = 0
-    
+
     return samples_dict
 
-# Runs the files to create a list of dicts which contain info about
-# the runs. Returns -1 if it failed
+
 def process_files(files):
+    """
+    Goes through the files and returns a list of dicts which contain info about
+    the runs. Returns -1 if it failed.
+    """
     samples = []
     if files:
         try:
@@ -178,28 +184,35 @@ def process_files(files):
         return samples
     return -1
 
-# Clean up on a bunch of files to generate CSVs and a report
+
 def clean_up(samples, files):
+    """
+    Clean up on a bunch of files to generate CSVs and a report.
+    """
     directory = files[0].split('/')
     path = directory[len(directory) - 2]
     df = pd.DataFrame(samples)
-    sorted_cols = ['file name', 'ms1 ppm', 'ms1 ppm weighted mean', 'ms1 intensity', 'ms1 max intensity']
-    sorted_cols.extend(['rt', 'rt at max intensity', 'ms2_precursor ppm', 'ms2_precursor ppm weighted mean'])
-    sorted_cols.extend(['ms2 ppm', 'ms2 ppm weighted mean', 'acquisition timestamp', 'acquisition time'])
+    sorted_cols = ['file name', 'ms1 ppm', 'ms1 ppm weighted mean',
+                   'ms1 intensity', 'ms1 max intensity']
+    sorted_cols.extend(['rt', 'rt at max intensity', 'ms2_precursor ppm',
+                        'ms2_precursor ppm weighted mean'])
+    sorted_cols.extend(['ms2 ppm', 'ms2 ppm weighted mean',
+                        'acquisition timestamp', 'acquisition time'])
 
     # get the full report
-    df = df.loc[:,sorted_cols]
+    df = df.loc[:, sorted_cols]
     df = df.sort_values('file name')
-    # This directory should be modular in case we decide to move it
-    csv_name = "/project/projectdirs/metatlas/projects/istd_logs/%s_run.csv" % path
-    df.to_csv(csv_name, index = False)
-    
+    csv_name = save_path + '%s_run.csv' % path
+    df.to_csv(csv_name, index=False)
+
     print 'begin clean up'
     # abridged report
     df = df.sort_values('file name')
-    sorted_cols = ['file name', 'acquisition time', 'ms1 ppm weighted mean', 'ms1 max intensity', 'rt at max intensity']
-    sorted_cols.extend(['ms2_precursor ppm weighted mean', 'ms2 ppm weighted mean'])
-    df = df.loc[:,sorted_cols]
+    sorted_cols = ['file name', 'acquisition time', 'ms1 ppm weighted mean',
+                   'ms1 max intensity', 'rt at max intensity']
+    sorted_cols.extend(['ms2_precursor ppm weighted mean',
+                        'ms2 ppm weighted mean'])
+    df = df.loc[:, sorted_cols]
     for col in sorted_cols:
         if 'weighted mean' in col or 'rt at max' in col:
             for i in range(df.shape[0]):
@@ -215,46 +228,59 @@ def clean_up(samples, files):
     print 'done clean up'
     return [csv_name, (df_non_blanks, df_blanks)]
 
-# Calculates the weighted mean for some ppm and intensity data
+
 def weighted_mean(df):
+    """
+    Calculates the weighted mean for some ppm and intensity data.
+    """
     i_sum = df['i'].sum()
     if i_sum:
         vm = df['i'].dot(df['ppm'])
         return vm / i_sum
     return -1
 
-def convert_to_html(df):
-    pd.options.display.max_colwidth = -1
-    html_table = df.to_html(index = False)
-    # hacky way to get css in the file
-    html_table = html_table.replace('<table border="1"', '<table style="width: 100%;')
-    html_table = html_table.replace('<th>', '<th style="border-bottom: 1px solid #ddd; padding: 8px; text-align: left; margin: 0">')
-    html_table = html_table.replace('<td>', '<td style="border-bottom: 1px solid #ddd; padding: 8px; text-align: left; margin: 0">')
-    html_table = html_table.replace('<tr style="text-align: right;">', '<tr style="text-align: left">')
-    return html_table
 
-# make emails more modular
-# sends an email of the run(s) done
+def convert_to_html(df):
+    """
+    Converts a dataframe into a styled html table for emails.
+    """
+    pd.options.display.max_colwidth = -1
+    html = df.to_html(index=False)
+    # hacky way to get css in the file
+    html = html.replace('<table border="1"',
+                        '<table style="width: 100%;')
+    html = html.replace('<th>',
+                        '<th style="border-bottom: 1px solid #ddd; padding: 8px; text-align: left; margin: 0">')
+    html = html.replace('<td>',
+                        '<td style="border-bottom: 1px solid #ddd; padding: 8px; text-align: left; margin: 0">')
+    html = html.replace('<tr style="text-align: right;">',
+                        '<tr style="text-align: left">')
+    return html
+
+
 def send_run_email(username, run_file, tables, standard='ABMBA'):
+    """
+    Sends an email of the run(s) done and packages the generated CSV(s).
+    """
     sender = 'pasteur@nersc.gov'
-    receivers = ['%s@nersc.gov' % username] #, '%s@nersc.gov' % 'bpb']
+    receivers = ['%s@nersc.gov' % username, '%s@nersc.gov' % 'bpb']
     # Build email
     msg = MIMEMultipart()
     msg['From'] = sender
-    msg['To'] = ", ".join(receivers) # needs to be a string
+    msg['To'] = ", ".join(receivers)  # needs to be a string
     msg['Subject'] = "Run Information"
-    
+
     template = "Hello %s,\n\n" % username
     template += "Here is a daily summary of your runs with %s from the last 3 days.\n" % standard
     template += "0's in the table indicate that the particular value was not detected."
     template += "Attached is a more verbose summary of the run in a .csv file.\n"
     # add more to the template if needed?
-    
+
     text = MIMEText(template, 'plain')
     msg.attach(text)
-    
+
     for title, table in itertools.izip(run_file, tables):
-        s = title.replace('run_data/','').replace('_run.csv', '\n')
+        s = title.replace('run_data/', '').replace('_run.csv', '\n')
         num_files = len(table[0]) + len(table[1])
         txt = '<h2 style="border-top: solid; padding-top: 10px;">Run Report for %s:</h2>' % s
         txt += '<p>Detected %d runs!</p>' % num_files
@@ -263,12 +289,12 @@ def send_run_email(username, run_file, tables, standard='ABMBA'):
         table_non_blank = convert_to_html(table[0])
         html = MIMEText(table_non_blank, 'html')
         msg.attach(html)
-        text = MIMEText('<h3>Blanks in %s:</h3>' %s, 'html')
+        text = MIMEText('<h3>Blanks in %s:</h3>' % s, 'html')
         msg.attach(text)
         table_blanks = convert_to_html(table[1])
         html = MIMEText(convert_to_html(table[1]), 'html')
         msg.attach(html)
-    
+
     for f in run_file:
         ctype, encoding = mimetypes.guess_type(f)
         if ctype is None or encoding is not None:
@@ -283,9 +309,10 @@ def send_run_email(username, run_file, tables, standard='ABMBA'):
         fp.close()
         encoders.encode_base64(attachment)
         # another case where save path needs to be modular / in a .yml
-        attachment.add_header("Content-Disposition", "attachment", filename=f.replace('/project/projectdirs/metatlas/projects/istd_logs/', ''))
+        attachment.add_header("Content-Disposition", "attachment",
+                              filename=f.replace(save_path, ''))
         msg.attach(attachment)
-    
+
     try:
         smtpObj = smtplib.SMTP('localhost')
         smtpObj.sendmail(sender, receivers, msg.as_string())
@@ -295,18 +322,21 @@ def send_run_email(username, run_file, tables, standard='ABMBA'):
         sys.stderr.write("Error: unable to send email to %s\n" % username)
         sys.stdout.flush()
 
+
 # Run this to start the task
 def run_checker():
     print 'starting task'
     start_time = time.time()
     origin_directory = '/project/projectdirs/metatlas/raw_data/'
-    #pull users, email them their run info
-    delta = 60*24*3 # 3 days to minutes
-    check = 'find %s -name "*.h5" -mmin -%d -size +2k' % (origin_directory, delta)
+    # pull users, email them their run info
+    delta = 60*24*3  # 3 days to minutes
+    check = 'find %s -name "*.h5" -mmin -%d -size +2k' % (
+            origin_directory, delta)
     valid_files = check_output(check, shell=True).decode('utf-8').splitlines()
     valid_files = set(valid_files)
 
-    files =  defaultdict(set) # dict of user name and the directory of their files
+    # dict of user name and the directory of their files
+    files = defaultdict(set)
     for i in valid_files:
         s = i.split('/')
         usr = s[-3]
@@ -323,27 +353,31 @@ def run_checker():
             print 'looking at', directory
             extension = '%s/%s/' % (username, directory)
             # list of all files in a given directory
-            dir_files = glob.glob(os.path.join(origin_directory + extension,'*.h5'))
+            dir_files = glob.glob(os.path.join(origin_directory +
+                                  extension, '*.h5'))
             # contains a list of dicts
             samples = process_files(dir_files)
-            # a list containing a csv name and a tuple of the non-blank, blank dataframes
+            # a list containing a csv name
+            # and a tuple of the non-blank, blank dataframes
             info = clean_up(samples, dir_files)
             csvs.append(info[0])
             html_tables.append((info[1][0], info[1][1]))
         print 'attempting to send email'
         send_run_email('mtrinh', csvs, html_tables)
-        print '\n' # for formatting purposes
+        print '\n'  # for formatting purposes
 
     print 'finished task in %s seconds' % (time.time() - start_time)
 
 # Integrity monitor does two major things:
-# 
+# - Checks the database for invalid files and warns the user.
+# - Runs through all the raw data collected in the past 3 days and gives the
+#   user a general report of the data collected.
 if __name__ == '__main__':
     check_metatlas()
 
-    runs = metob.retrieve('lcmsruns',username='*')
+    runs = metob.retrieve('lcmsruns', username='*')
 
     for i in runs:
         run_times[i.hdf5_file] = i.acquisition_time
-    
+
     run_checker()
