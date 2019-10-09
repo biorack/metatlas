a028060fb9c130c4695302ad0c0c2eb37ad5f8f0
Ben Bowen <bpbowen@lbl.gov>
2019-10-09 15:13:57 -0700

    fixed reset_index due to duplicate column names

--- metatlas/helpers/fastanalysis.py
+++ metatlas/helpers/fastanalysis.py
@@ -182,6 +182,7 @@ def make_scores_df(metatlas_dataset):
     scores = []
 
     msms_hits_df = dp.get_msms_hits(metatlas_dataset, ref_index=['database', 'id', 'inchi_key', 'precursor_mz'])
+    msms_hits_df.rename(columns={'inchi_key':'inchi_key_2'},inplace=True)
     msms_hits_df.reset_index(inplace=True)
 
     for compound_idx in range(len(compound_names)):
