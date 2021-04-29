df39b846ea1924126ed7d687f220c7a0fb5ae0ca
Will Holtz <wholtz@gmail.com>
2021-04-29 16:18:56 -0700

    Add GitHub Actions for CI, add system test

--- noxfile.py
+++ noxfile.py
@@ -1,25 +1,42 @@
+# pylint: disable=missing-function-docstring
+
 import nox
 
-py_versions = ['3.8']
+py_versions = ['3.8', '3.9']
+
+nox.options.sessions = ["flake8_diff", "pylint", "unit_tests-3.8"]
+
+single_py_args = {'venv_backend': 'conda', 'reuse_venv': False}
+multi_py_args = {**single_py_args, **{'python': py_versions}}
 
 nox.options.error_on_external_run = True
 
-full_env_args = {'venv_backend': 'conda', 'reuse_venv': False, 'python': py_versions}
+run_deps = [
+    'dill=0.3.3',
+    'gspread=3.7.0',
+    'hdf5=1.10.6',
+    'ipywidgets=7.6.3',
+    'jupyterlab=3.0.14',
+    'matplotlib=3.4.1',
+    'oauth2client=4.1.3',
+    'pandas=1.2.4',
+    'pip=21.1',
+    'pymysql=1.0.2',
+    'pymzml=2.4.7',
+    'pytables=3.6.1',
+    'pyyaml=5.4.1',
+    'rdkit=2021.03.1',
+    'simplejson=3.17.2',
+    'sqlalchemy=1.4.11',
+    'tabulate=0.8.9',
+    'xlsxwriter=1.4.0'
+]
+
+channel_names = ['conda-forge', 'bioconda']
+channels = [f"--channel={name}" for name in channel_names]
 
 
-@nox.session(**full_env_args)
-def pylint(session):
-    session.conda_install('--file', 'metatlas_env.yml')
-    session.run('pylint', 'metatlas')
-
-
-@nox.session(**full_env_args)
-def test(session):
-    session.conda_install('--file', 'metatlas_env.yml')
-    session.run('pytest', '--cov', 'metatlas')
-
-
-@nox.session(reuse_venv=True)
+@nox.session(python=py_versions[0])
 def flake8_diff(session):
     session.install('flake8',
                     'flake8-bugbear',
@@ -28,7 +45,43 @@ def flake8_diff(session):
     session.run('sh', '-c', 'git diff -U0 --staged HEAD | flake8 --diff', external=True)
 
 
-@nox.session(reuse_venv=True)
+@nox.session(python=py_versions[0])
+def flake8(session):
+    session.install('flake8',
+                    'flake8-bugbear',
+                    'flake8-builtins',
+                    'flake8-comprehensions')
+    session.run('flake8', 'metatlas', 'tests')
+
+
+@nox.session(python=py_versions[0])
+def black(session):
+    session.install('black')
+    session.run('black', '--check', '--diff', '--color', 'metatlas', 'tests')
+
+
+@nox.session(python=py_versions[0])
+def pylint(session):
+    session.install('pylint')
+    session.run('pylint', 'metatlas', 'tests')
+
+
+@nox.session(venv_backend='conda', python=py_versions, reuse_venv=True)
+def unit_tests(session):
+    test_deps = ['pytest=6.2.3', 'pytest-cov=2.11.1']
+    session.conda_install(*(channels+run_deps+test_deps))
+    session.install('--no-deps', 'dataset==1.5.0')
+    session.run('pytest', '--cov', 'tests/unit/')
+
+
+@nox.session(python=py_versions[0])
+def system_tests(session):
+    test_deps = ['pytest==6.2.3']
+    session.install(*test_deps)
+    session.run('pytest', 'tests/system/')
+
+
+@nox.session(python=py_versions[0])
 def install_git_hooks(session):
     session.install('pre-commit')
     session.run('pre-commit', 'install')
---
+++ tests/system/test_targeted.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+
+
+def test_targeted_by_line(tmp_path):
+    image = 'registry.spin.nersc.gov/metatlas_test/metatlas_ci01:v1.0.0'
+    experiment = '20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583'
+    out_file = tmp_path/experiment/'root0/FinalEMA-HILIC/POS_data_sheets/POS_peak_height.tab'
+    expected = [
+        f"group\t{experiment}_POS_MSMS_root0_Cone-S1\t{experiment}_POS_MSMS_root0_Cone-S2\t{experiment}_POS_MSMS_root0_Cone-S3\t{experiment}_POS_MSMS_root0_Cone-S4",  # noqa: E501
+        f"file\t{experiment}_POS_MSMS_49_Cone-S1_1_Rg70to1050-CE102040-QlobataAkingi-S1_Run34.h5\t{experiment}_POS_MSMS_57_Cone-S2_1_Rg70to1050-CE102040-QlobataAkingi-S1_Run40.h5\t{experiment}_POS_MSMS_65_Cone-S3_1_Rg70to1050-CE102040-QlobataAkingi-S1_Run16.h5\t{experiment}_POS_MSMS_73_Cone-S4_1_Rg70to1050-CE102040-QlobataAkingi-S1_Run31.h5",  # noqa: E501
+        'short groupname\tPOS_Cone-S1\tPOS_Cone-S2\tPOS_Cone-S3\tPOS_Cone-S4',
+        'sample treatment\tCone-S1\tCone-S2\tCone-S3\tCone-S4',
+        'short filename\t20201106_PS-KM_OakGall_final_HILICZ_POS_Rg70to1050-CE102040-QlobataAkingi-S1\t20201106_PS-KM_OakGall_final_HILICZ_POS_Rg70to1050-CE102040-QlobataAkingi-S1\t20201106_PS-KM_OakGall_final_HILICZ_POS_Rg70to1050-CE102040-QlobataAkingi-S1\t20201106_PS-KM_OakGall_final_HILICZ_POS_Rg70to1050-CE102040-QlobataAkingi-S1',  # noqa: E501
+        'short samplename\tPOS_Cone-S1_1_Rg70to1050-CE102040-QlobataAkingi-S1\tPOS_Cone-S2_1_Rg70to1050-CE102040-QlobataAkingi-S1\tPOS_Cone-S3_1_Rg70to1050-CE102040-QlobataAkingi-S1\tPOS_Cone-S4_1_Rg70to1050-CE102040-QlobataAkingi-S1',  # noqa: E501
+        '0000_2deoxyadenosine_positive_M+H252p1091_2p20\t304761.90625\t416788.03125\t837662.0625\t2359861.25',
+        '0001_adenine_positive_M+H136p0618_2p52\t1645281.875\t12096485.0\t51774956.0\t91955488.0',
+        '0002_adenine_positive_M+H136p0618_2p52\t1880780.125\t12096485.0\t51774956.0\t91955488.0',
+        '0003_xanthine_positive_M+H153p0407_2p70\t72926.875\t60128.625\t231272.640625\t317968.03125',
+        '0004_4-pyridoxic_acid_positive_M+H184p0605_2p84\t44113.671875\t66073.203125\t94702.390625\t214180.296875',  # noqa: E501
+        '0005_adenosine_positive_M+H268p1041_3p02\t26611868.0\t119774184.0\t267718880.0\t473905024.0',
+        ''
+    ]
+    subprocess.run(["docker", "run",
+                    "-v", f"{os.getcwd()}:/src",
+                    "-v", f"{tmp_path}:/out",
+                    image,
+                    "papermill",
+                    "-p", "experiment", experiment,
+                    "-p", "metatlas_repo_path", "/src",
+                    "-p", "project_directory", "/out",
+                    "-p", "max_cpus", "2",
+                    "/src/notebooks/reference/Targeted.ipynb",
+                    "/out/Targeted.ipynb"],
+                   check=True)
+    with open(out_file, 'r') as handle:
+        for num, line in enumerate(handle.readlines()):
+            clean_line = line.rstrip('\n')
+            assert expected[num] == clean_line
