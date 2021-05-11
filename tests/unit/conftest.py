7ef79f2a52f7ed8387c9f2d869fc8c621e33e805
Will Holtz <wholtz@gmail.com>
2021-05-11 08:42:58 -0700

    Improvements to testing
    docker/metatlas_env.yaml is now the sole location for dependencies.
    Enabled black, pylint, and flake8 on selected files.

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,7 +1,12 @@
+"""
+per-directory pytest configuration
+This makes the fixtures available to tests within this directory
+"""
 from glob import glob
 
 
 def refactor(string: str) -> str:
+    """python file path to module name converter"""
     return string.replace("/", ".").replace("\\", ".").replace(".py", "")
 
 
---
+++ tests/unit/test_metatlas_dataset.py
@@ -0,0 +1,3 @@
+""" tests for MetatasDataset """
+# pylint: disable=missing-function-docstring
+# from metatlas.datastructures import metatlas_dataset as mads
