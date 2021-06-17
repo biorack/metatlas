1e8eff48cad82a7f3f867ddea45896c0c51efdcd
Will Holtz <wholtz@gmail.com>
2021-06-17 08:53:25 -0700

    WIP - add analysis_setup.sh script

---
+++ metatlas/scripts/analysis_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+if [[ $# -ne 3 ]]; then
+	echo "Usage: $0 metatlas_repo_dir base_output_dir experiment_id"
+	exit 1
+fi
+
+REPO_DIR="$1"
+OUT_DIR="$2"
+EXP="$3"
+
+DATA_DIR="/project/projectdirs/metatlas/raw_data/akuftin/${EXP}"
+
+if [ ! -d "${DATA_DIR}" ]; then
+	echo "ERROR: could not find data directory ${DATA_DIR}." >&2
+	exit 2
+fi
+
+IFS='_' read -ra EXP_ARRAY <<< "$EXP"
+NOTEBOOK_BASE="${EXP_ARRAY[3]}_${EXP_ARRAY[4]}"
+
+mkdir "$OUT_DIR"
+cp "${REPO_DIR}/notebooks/reference/Workflow_Notebook_VS_Auto_RT_Predict_V2.ipynb" "${OUT_DIR}/${NOTEBOOK_BASE}_RT_Predict.ipynb"
+cp "${REPO_DIR}/notebooks/reference/Targeted.ipynb" "${OUT_DIR}/${NOTEBOOK_BASE}.ipynb"
