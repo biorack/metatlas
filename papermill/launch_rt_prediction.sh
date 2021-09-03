7b1b6552641b08f9fa8660599016d186960fe89f
Will Holtz <wholtz@gmail.com>
2021-09-03 16:22:37 -0700

    Install kernel in RT adjust slurm script

--- papermill/launch_rt_prediction.sh
+++ papermill/launch_rt_prediction.sh
@@ -14,6 +14,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_DIR="$(dirname "$SCRIPT_DIR")"
 EXP_DIR="${PROJECT_DIR}/$EXP"
 ANALYSIS_DIR="${EXP_DIR}/${USER}${ANALYSIS_NUM}"
+KERNEL_SOURCE="${SCRIPT_DIR}/notebooks/kernels/metatlas-targeted.kernel.json"
+KERNEL_DESTINATION="${HOME}/.local/share/jupyter/kernels/metatlas-targeted/kernel.json"
 
 IFS='_' read -ra TOKENS <<< "$EXP"
 PROPOSAL="${TOKENS[3]}"
@@ -21,6 +23,9 @@ PROPOSAL="${TOKENS[3]}"
 export IN_FILE="${REPO_DIR}/notebooks/reference/RT_Prediction.ipynb"
 export OUT_FILE="${ANALYSIS_DIR}/${PROPOSAL}_RT_Prediction_papermill.ipynb"
 export PARAMETERS="-p experiment $EXP -p metatlas_repo_path $REPO_DIR -p project_directory $PROJECT_DIR -p max_cpus 32 -p analysis_number $ANALYSIS_NUM"
+
+mkdir -p "${HOME}/.local/share/jupyter/kernels/metatlas-targeted"
+cp "$KERNEL_SOURCE" "$KERNEL_DESTINATION"
 
 mkdir -p "$ANALYSIS_DIR"
 sbatch -J "${PROPOSAL}_RT_Pred" "${REPO_DIR}/papermill/slurm_template.sh"
