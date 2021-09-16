d439b4b27df41b0135aa922259337ed1f0fead7a
Will Holtz <wholtz@gmail.com>
2021-09-16 09:52:44 -0700

    Allow source_atlas to be owned by someone else

--- metatlas/datastructures/metatlas_dataset.py
+++ metatlas/datastructures/metatlas_dataset.py
@@ -636,7 +636,7 @@ class MetatlasDataset(HasTraits):
 
     def _clone_source_atlas(self) -> metob.Atlas:
         logger.info("Retriving source atlas: %s", self.ids.source_atlas)
-        source_atlas = get_atlas(cast(AtlasName, self.ids.source_atlas), self.ids.username)
+        source_atlas = get_atlas(cast(AtlasName, self.ids.source_atlas), "*")
         source_atlas_df = ma_data.make_atlas_df(source_atlas)
         logger.info("Cloning atlas %s", self.ids.source_atlas)
         return dp.make_atlas_from_spreadsheet(
