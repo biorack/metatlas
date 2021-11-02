0e0244fe99b1cd66116b917ac028b459720609e5
Will Holtz <wholtz@gmail.com>
2021-11-01 22:52:40 -0700

    Alter RT Predict slurm job to use shifter

--- papermill/launch_rt_prediction.sh
+++ papermill/launch_rt_prediction.sh
@@ -22,21 +22,15 @@ ANALYSIS_NUM="$2"
 PROJECT_DIR="$3"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export REPO_DIR="$(dirname "$SCRIPT_DIR")"
 EXP_DIR="${PROJECT_DIR}/$EXP"
 ANALYSIS_DIR="${EXP_DIR}/${USER}${ANALYSIS_NUM}"
-KERNEL_SOURCE="${REPO_DIR}/notebooks/kernels/metatlas-targeted.kernel.json"
-KERNEL_DESTINATION="${HOME}/.local/share/jupyter/kernels/metatlas-targeted/kernel.json"
 
 IFS='_' read -ra TOKENS <<< "$EXP"
 PROPOSAL="${TOKENS[3]}"
 
-export IN_FILE="${REPO_DIR}/notebooks/reference/RT_Prediction.ipynb"
+export IN_FILE="/src/notebooks/reference/RT_Prediction.ipynb"
 export OUT_FILE="${ANALYSIS_DIR}/${PROPOSAL}_RT_Prediction_papermill.ipynb"
-export PARAMETERS="-p experiment $EXP -p metatlas_repo_path $REPO_DIR -p project_directory $PROJECT_DIR -p max_cpus 32 -p analysis_number $ANALYSIS_NUM"
-
-mkdir -p "${HOME}/.local/share/jupyter/kernels/metatlas-targeted"
-cp "$KERNEL_SOURCE" "$KERNEL_DESTINATION"
+export PARAMETERS="-p experiment $EXP -p project_directory $PROJECT_DIR -p max_cpus 32 -p analysis_number $ANALYSIS_NUM"
 
 mkdir -p "$ANALYSIS_DIR"
-sbatch -J "${PROPOSAL}_RT_Pred" "${REPO_DIR}/papermill/slurm_template.sh"
+sbatch -J "${PROPOSAL}_RT_Pred" "${SCRIPT_DIR}/slurm_template.sh"
--- papermill/slurm_template.sh
+++ papermill/slurm_template.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-#SBATCH -N 1
-#SBATCH -C haswell
+#SBATCH --image=docker:wjhjgi/metatlas_shifter:latest
+#SBATCH --constraint=haswell
 #SBATCH --account=gtrnd
 #SBATCH --qos=genepool
 #SBATCH --licenses=cfs
 #SBATCH --mail-type=ALL
-#SBATCH -t 12:00:00
+#SBATCH --time=12:00:00
 
 #OpenMP settings:
 export OMP_NUM_THREADS=1
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
-CONDA_DIR="$(dirname "$(dirname "$(grep 'metatlas-targeted' "${REPO_DIR}/notebooks/kernels/metatlas-targeted.kernel.json" | cut -d\" -f 2)")")"
 date
 echo "input file: $IN_FILE"
 echo "output file: $OUT_FILE"
-module load python/3.8-anaconda-2020.11
-eval "$(conda shell.bash hook)"
-conda activate "$CONDA_DIR"
 
-srun -n 1 -c 64 --cpu_bind=cores papermill "$IN_FILE" "$OUT_FILE" $PARAMETERS
+shifter /usr/local/bin/papermill -k papermill "$IN_FILE" "$OUT_FILE" $PARAMETERS
