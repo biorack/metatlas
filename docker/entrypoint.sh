06c95d58901d1b1d4732adc8652242314771efa9
Will Holtz <wholtz@gmail.com>
2021-11-03 11:26:15 -0700

    use mktemp to create src dir in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-cp -R "$1" "$2"
+WORK_DIR="$(mktemp -d /tmp/metatlas.XXXXXXXXXX)"
 
-shift
+if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+  echo "Could not create temp dir"
+  exit 1
+fi
+
+export PYTHONPATH="${WORK_DIR}:${PYTHONPATH}"
+
+function cleanup {
+  rm -rf "$WORK_DIR"
+}
+
+trap cleanup EXIT
+
+cp -a "$1/." "$WORK_DIR"
+
 shift
 
 exec "$@"
