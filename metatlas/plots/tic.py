399ada6df5efda1e7be47be4cc6ccb4e43428d08
Will Holtz <wholtz@gmail.com>
2021-11-18 10:44:03 -0800

    TIC plots from FPS files now use current polarity

--- metatlas/plots/tic.py
+++ metatlas/plots/tic.py
@@ -21,15 +21,16 @@ MetatlasDataset = List[List[Any]]  # avoiding a circular import
 class Tic(plot_set.Plot):
     """TIC for a single sample"""
 
-    def __init__(self, title: str, group_name: str, h5_file_name: str):
+    def __init__(self, title: str, group_name: str, h5_file_name: str, polarity: str):
         super().__init__(title, group_name)
         self.h5_file_name = h5_file_name
+        assert polarity in ["positive", "negative"]
+        self.polarity = polarity
 
     def plot(self, ax: matplotlib.axes.Axes, back_color: utils.Color = "white") -> None:
         """Draw plot of TIC on ax"""
         super().plot(ax, back_color)
-        dataset = "ms1_" + Path(self.h5_file_name).stem.split("_")[9].lower()
-        assert dataset in ["ms1_pos", "ms1_neg"]
+        dataset = f"ms1_{self.polarity[:3]}"
         tic_df = get_bpc(self.h5_file_name, dataset=dataset, integration="tic")
         ax.plot(tic_df["rt"], tic_df["i"])
         ax.xaxis.set_minor_locator(MultipleLocator(1))
@@ -37,6 +38,7 @@ class Tic(plot_set.Plot):
 
 def save_sample_tic_pdf(
     data: MetatlasDataset,
+    polarity: str,
     file_name: str,
     overwrite: bool = False,
     sharey: bool = True,
@@ -46,13 +48,14 @@ def save_sample_tic_pdf(
     max_plots_per_page: int = 30,
 ) -> None:
     """Generate a PDF of TIC plots for samples within data"""
+    assert polarity in ["positive", "negative"]
     file_order_tics = []
     for sample in data:
         fields = Path(sample[0]["lcmsrun"].name).stem.split("_")
         title = f"{fields[0]}_{fields[11]}_{fields[13]}_{fields[15]}"
         group_name = sample[0]["group"].short_name
         h5_file_name = sample[0]["lcmsrun"].hdf5_file
-        file_order_tics.append(Tic(title, group_name, h5_file_name))
+        file_order_tics.append(Tic(title, group_name, h5_file_name, polarity))
     tics = sorted(file_order_tics, key=lambda x: (x.group_name, x.title))
     pdf_title = "TICs"
     plots = plot_set.PlotSet(tics, max_plots_per_page, x_min=x_min, x_max=x_max, y_min=y_min, sharey=sharey)
