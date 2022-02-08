dcc2e0213c466dfe532e69c6a6a5fdc2b868afb2
Will Holtz <wholtz@gmail.com>
2022-02-08 14:43:59 -0800

    configure for C18 (#464)

--- metatlas/tools/predict_rt.py
+++ metatlas/tools/predict_rt.py
@@ -51,11 +51,11 @@ TEMPLATES = {
 QC_ATLASES = {
     "positive": {
         "HILICZ": {"atlas": "HILICz150_ANT20190824_TPL_QCv3_Unlab_POS", "username": "vrsingan"},
-        "C18": {"atlas": "C18_20220125_QC_POS", "username": "wjholtz"},
+        "C18": {"atlas": "C18_20220208c_QC_POS", "username": "wjholtz"},
     },
     "negative": {
         "HILICZ": {"atlas": "HILICz150_ANT20190824_TPL_QCv3_Unlab_NEG", "username": "vrsingan"},
-        "C18": {"atlas": "C18_20220125_QC_NEG", "username": "wjholtz"},
+        "C18": {"atlas": "C18_20220208c_QC_NEG", "username": "wjholtz"},
     },
 }
 
