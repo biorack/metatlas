9b6bfe6903e6fa87c65cb9d4fa3ed86c0e9ea561
Will Holtz <wholtz@gmail.com>
2022-02-09 14:44:28 -0800

    mzml to h5 file converter improvements
    moves mzml files that fails conversion
    on error, try to email the person who generated the file
    general clean up, linting fixes, and minor refactoring

--- metatlas/io/file_converter.py
+++ metatlas/io/file_converter.py
@@ -1,118 +1,100 @@
-from __future__ import absolute_import
+""" mzml to h5 file conversion """
+
+import argparse
 import fcntl
-import multiprocessing as mp
 import os
-import pwd
-import random
 import re
 import shutil
-import smtplib
+import subprocess
 import sys
 import time
 import traceback
 
-# from collections import defaultdict
-from datetime import datetime, time as dtime
+from datetime import datetime
 from subprocess import check_output
 
 from metatlas.datastructures.metatlas_objects import LcmsRun, store, retrieve
 from metatlas.io.mzml_loader import mzml_to_hdf
-
 from metatlas.io.mzml_loader import VERSION_TIMESTAMP
 from metatlas.io.system_utils import send_mail
 
-NPROC = 8
 
-manager = mp.Manager()
-readonly_files = manager.dict() # username (or uid) | a set of files associated with them
-other_errors = manager.dict() # info with user | list of error messages
+readonly_files = {}  # username (or uid) | a set of files associated with them
+other_errors = {}  # info with user | list of error messages
 patt = re.compile(r".+\/raw_data\/(?P<username>[^/]+)\/(?P<experiment>[^/]+)\/(?P<path>.+)")
 
+MEMBERS_CMD = "getent group metatlas | cut -d: -f4"
+USERS = sorted(subprocess.check_output(MEMBERS_CMD, shell=True, text=True).strip().split(','))
+
+
+def _file_name_to_username(file_name):
+    initials_field = os.path.basename(file_name).split("_")[1].lower()
+    initials = initials_field.split("-")[-1]
+    for user in USERS:
+        if user.startswith(initials):
+            return user
+    return None
+
+
+def _write(message, out_fh):
+    out_fh.write(message + "\n")
+    out_fh.flush()
+
+
+def _write_stdout(message):
+    _write(message, sys.stdout)
+
+
+def _write_stderr(message):
+    _write(message, sys.stderr)
+
+
 def get_acqtime_from_mzml(mzml_file):
-    startTimeStamp=None
-    with open(mzml_file) as mzml:
+    start_time = None
+    with open(mzml_file, "r", encoding="utf-8") as mzml:
         for line in mzml:
-            if 'startTimeStamp' in line:
-                startTimeStamp = line.split('startTimeStamp="')[1].split('"')[0].replace('T',' ').rstrip('Z')
+            if 'start_time' in line:
+                start_time = line.split('start_time="')[1].split('"')[0].replace('T', ' ').rstrip('Z')
                 break
-#     print startTimeStamp
-    if not '-infinity' in startTimeStamp:
-        date_object = datetime.strptime(startTimeStamp, '%Y-%m-%d %H:%M:%S')
+    if '-infinity' not in start_time:
+        date_object = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
         utc_timestamp = int(time.mktime(date_object.timetuple()))
     else:
         utc_timestamp = int(0)
     return utc_timestamp
 
-# Helper function, converts a single file
-def convert(file):
-    ind = file[0]
-    fname = file[1]
 
-    sys.stdout.write('(%s): %s\n' % (ind + 1, fname))
-    sys.stdout.flush()
+def convert(ind, fname):
+    """Helper function, converts a single file"""
+    _write_stdout(f"({ind+1}): {fname}")
 
     # Get relevant information about the file.
+    username = _file_name_to_username(fname)
     info = patt.match(os.path.abspath(fname))
     if info:
         info = info.groupdict()
     else:
-        sys.stdout.write("Invalid path name: %s\n" % fname)
-        sys.stdout.flush()
+        _write_stdout(f"Invalid path name: {fname}")
         return
     dirname = os.path.dirname(fname)
-    try:
-        uid = os.stat(fname).st_uid
-        try:
-            username = pwd.getpwuid(uid).pw_name
-        except KeyError:
-            username = uid
-    except OSError:
-        try:
-            uid = os.stat(dirname).st_uid
-            try:
-                username = pwd.getpwuid(uid).pw_name
-            except KeyError:
-                username = uid
-        except Exception:
-            username = info['username']
-
     # Change to read only.
     try:
         os.chmod(fname, 0o660)
     except Exception as e:
-        sys.stderr.write(str(e) + '\n')
-        sys.stderr.flush()
-
-#     # Copy the original file to a pasteur backup.
-#     if os.environ['USER'] == 'pasteur':
-#         pasteur_path = fname.replace('raw_data', 'pasteur_backup')
-#         dname = os.path.dirname(pasteur_path)
-#         if not os.path.exists(dname):
-#             os.makedirs(dname)
-#         try:
-#             shutil.copy(fname, pasteur_path)
-#         except IOError as e:
-#             if (username not in readonly_files):
-#                 readonly_files[username] = set()
-#             readonly_files[username].add(dirname)
-#             return
-
+        _write_stderr(str(e))
     # Get a lock on the mzml file to prevent interference.
     try:
-        fid = open(fname, 'r')
+        fid = open(fname, 'r', encoding="utf-8")
         fcntl.flock(fid, fcntl.LOCK_EX | fcntl.LOCK_NB)
     except IOError:
         fid.close()
-        msg = '%s already converting in another process\n' % fname
-        sys.stderr.write(msg)
-        sys.stderr.flush()
+        _write_stderr(f"{fname} already converting in another process")
         return
 
     # Convert to HDF and store the entry in the database.
     try:
         hdf5_file = fname.replace('mzML', 'h5')
-        sys.stderr.write('hdf5file is: %s'%hdf5_file)
-        #Get Acquisition Time Here
+        _write_stderr(f"hdf5file is: {hdf5_file}")
         acquisition_time = get_acqtime_from_mzml(fname)
         mzml_to_hdf(fname, hdf5_file, True)
         os.chmod(hdf5_file, 0o660)
@@ -122,28 +104,34 @@ def convert(file):
         try:
             runs = retrieve('lcmsrun', username='*', mzml_file=fname)
         except Exception:
-            runs = list()
-        if not len(runs):
-            run = LcmsRun(name=info['path'], description=description,
-                          username=info['username'],
+            runs = []
+        if not runs:
+            run = LcmsRun(name=info['path'],
+                          description=description,
+                          username=username,
                           experiment=info['experiment'],
-                          creation_time=ctime, last_modified=ctime,
-                          mzml_file=fname, hdf5_file=hdf5_file, acquisition_time = acquisition_time)
+                          creation_time=ctime,
+                          last_modified=ctime,
+                          mzml_file=fname,
+                          hdf5_file=hdf5_file,
+                          acquisition_time=acquisition_time)
             store(run)
     except Exception as e:
         if 'exists but it can not be written' in str(e):
-            if (username not in readonly_files):
+            if username not in readonly_files:
                 readonly_files[username] = set()
             readonly_files[username].add(dirname)
         else:
             msg = traceback.format_exception(*sys.exc_info())
-            msg.insert(0, 'Cannot convert %s' % fname)
-            dat = info['username']
-            if (dat not in other_errors):
-                other_errors[info['username']] = list()
-            other_errors[info['username']].append('\n'.join(msg))
-        sys.stderr.write(str(e) + '\n')
-        sys.stderr.flush()
+            msg.insert(0, f"Cannot convert {fname}")
+            dat = username
+            if dat not in other_errors:
+                other_errors[username] = []
+            other_errors[username].append('\n'.join(msg))
+            fail_path = fname.replace('raw_data', 'conversion_failures')
+            _write_stderr(f"Moving file\n{fname}\nto\n{fail_path}\n")
+            shutil.move(fname, fail_path)
+        _write_stderr(str(e))
         try:
             os.remove(hdf5_file)
         except:
@@ -157,68 +145,35 @@ def update_metatlas(directory):
     Converts all files to HDF in metatlas. Emails the user if there was
     any kind of error with converting a file.
     """
-
-    # need to have processes sync files and errors through a manager
-    # readonly_files = defaultdict(set)
-    # other_errors = defaultdict(list)
-    #directory = os.path.abspath(directory)
-    
-    
-
-    # Sleep a random amount of time to avoid running at the same time as
-    # other processes.
-    time.sleep(random.random() * 2)
-    mzml_files = check_output('find %s -name "*.mzML"' % directory, shell=True)
+    mzml_files = check_output(f'find {directory} -name "*.mzML"', shell=True)
     mzml_files = mzml_files.decode('utf-8').splitlines()
 
     # Find valid h5 files newer than the format version timestamp.
     delta = int((time.time() - VERSION_TIMESTAMP) / 60)
-    check = 'find %s -name "*.h5" -mmin -%s -size +2k' % (directory, delta)
-    valid_files = check_output(check, shell=True).decode('utf-8').splitlines()
-    valid_files = set(valid_files)
+    check = f'find {directory} -name "*.h5" -mmin -{delta} -size +2k'
+    valid_files = set(check_output(check, shell=True).decode('utf-8').splitlines())
+    new_files = [file for file in mzml_files if file.replace('.mzML', '.h5') not in valid_files]
 
-    new_files = []
-    for mzml_file in mzml_files:
-        if mzml_file.replace('.mzML', '.h5') not in valid_files:
-            new_files.append(mzml_file)
-
-    if len(new_files) > 0:
-        sys.stdout.write('Found %s files\n' % len(new_files))
-        sys.stdout.flush()
-        files = list(enumerate(new_files))
-
-#         if sys.version_info[0] < 3: 
-#             pool = mp.Pool(min(NPROC,len(new_files)))
-#             pool.map(convert, files)
-#             pool.close()
-#             pool.join()
-#         else:
-#             # #Python3
-#             with mp.Pool(min(NPROC,len(new_files))) as pool:
-#                 pool.map(convert, files)
-        for ffff in files:
-            convert(ffff)
-
-
+    if new_files:
+        _write_stdout(f"Found {len(new_files)} files")
+        for ind, ffff in enumerate(new_files):
+            convert(ind, ffff)
         if readonly_files:
             for (username, dirnames) in readonly_files.items():
                 body = ("Please log in to NERSC and run 'chmod 777' on the "
-                       "following directories:\n%s" % ('\n'.join(dirnames)))
+                        "following directories:\n%s" % ('\n'.join(dirnames)))
                 send_mail('Metatlas Files are Inaccessible', username, body)
-
         if other_errors:
             for (username, errors) in other_errors.items():
-                body = 'Errored files found while loading in Metatlas files:\n\n%s' % '\n********************************\n'.join(errors)
+                body = ('Errored files found while loading in Metatlas files:\n\n%s' %
+                        '\n********************************\n'.join(errors))
                 send_mail('Errors loading Metatlas files', username, body)
-    sys.stdout.write('Done!\n')
-    sys.stdout.flush()
+    _write_stdout('Done!')
+
 
 if __name__ == '__main__':
-    import argparse
-
     parser = argparse.ArgumentParser(description="Watchdog to monitor directory for new files")
     parser.add_argument("directory", type=str, nargs=1, help="Directory to watch")
     args = parser.parse_args()
-    sys.stdout.write(str(args) + '\n')
-    sys.stdout.flush()
+    _write_stdout(str(args))
     update_metatlas(args.directory[0])
