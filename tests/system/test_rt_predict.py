db4e82f50511600b083237f12bd62eee666e9727
Will Holtz <wholtz@gmail.com>
2022-05-02 11:03:06 -0700

    test csv outputs by dataframes

--- tests/system/test_rt_predict.py
+++ tests/system/test_rt_predict.py
@@ -1,5 +1,7 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, line-too-long, duplicate-code
 
+import numpy as np
+
 from . import utils
 
 
@@ -19,19 +21,55 @@ Polynomial model with intercept=0.733 and coefficents=[0.00000, 0.81321, 0.00919
 groups = 20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_FPS_MS1_root0_QC, 20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_POS_MSMS_root0_QC
 atlas = HILICz150_ANT20190824_TPL_QCv3_Unlab_POS
 """
-    expected[
-        str(tmp_path / experiment / "root0/data_QC/RT_Predicted_Model_Comparison.csv")
-    ] = """,RT Measured,RT Reference,RT Linear Pred,RT Polynomial Pred,RT Diff Linear,RT Diff Polynomial
-0000_uracil_unlabeled_positive_M+H113p0346_1p39,1.884217e+00,1.393700e+00,1.761967e+00,1.884217e+00,1.222508e-01,-1.865175e-14
-0001_cytosine_unlabeled_positive_M+H112p0505_4p83,4.878586e+00,4.833664e+00,5.049671e+00,4.878586e+00,-1.710854e-01,1.776357e-15
-0002_sucrose_unlabeled_positive_M+H343p1235_13p45,1.332883e+01,1.344515e+01,1.328000e+01,1.332883e+01,4.883459e-02,1.776357e-14"""
+    expected_df = {}
+    expected_df[str(tmp_path / experiment / "root0/data_QC/RT_Predicted_Model_Comparison.csv")] = {
+        "Unnamed: 0": {
+            0: "0000_uracil_unlabeled_positive_M+H113p0346_1p39",
+            1: "0001_cytosine_unlabeled_positive_M+H112p0505_4p83",
+            2: "0002_sucrose_unlabeled_positive_M+H343p1235_13p45",
+        },
+        "RT Measured": {0: 1.884217, 1: 4.878586, 2: 13.32883},
+        "RT Reference": {0: 1.3937, 1: 4.833664, 2: 13.44515},
+        "RT Linear Pred": {0: 1.761967, 1: 5.049671, 2: 13.28},
+        "RT Polynomial Pred": {0: 1.884217, 1: 4.878586, 2: 13.32883},
+        "RT Diff Linear": {0: 0.1222508, 1: -0.1710854, 2: 0.04883459},
+        "RT Diff Polynomial": {0: -1.865175e-14, 1: 1.776357e-15, 2: 1.776357e-14},
+    }
 
-    expected[
-        str(tmp_path / experiment / "root0/data_QC/QC_Measured_RTs.csv")
-    ] = """,20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_FPS_MS1_0_QC_Pre_Rg70to1050-CE102040--QC_Run6.h5,20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_POS_MSMS_0_QC_Pre_Rg70to1050-CE102040--QC_Run7.h5,20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_FPS_MS1_0_QC_Post_Rg70to1050-CE102040--QC_Run307.h5,20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_POS_MSMS_0_QC_Post_Rg70to1050-CE102040--QC_Run308.h5,mean,median,min,max,standard deviation,standard error,#NaNs
-0000_uracil_unlabeled_positive_M+H113p0346_1p39,1.879153e+00,1.889282e+00,1.878133e+00,1.892506e+00,1.884768e+00,1.884217e+00,1.878133e+00,1.892506e+00,7.206614e-03,3.603307e-03,0
-0001_cytosine_unlabeled_positive_M+H112p0505_4p83,4.836015e+00,4.846456e+00,4.910717e+00,4.921309e+00,4.878624e+00,4.878586e+00,4.836015e+00,4.921309e+00,4.359777e-02,2.179888e-02,0
-0002_sucrose_unlabeled_positive_M+H343p1235_13p45,,,1.334303e+01,1.331463e+01,1.332883e+01,1.332883e+01,1.331463e+01,1.334303e+01,2.008078e-02,1.419926e-02,0"""
+    expected_df[str(tmp_path / experiment / "root0/data_QC/QC_Measured_RTs.csv")] = {
+        "Unnamed: 0": {
+            0: "0000_uracil_unlabeled_positive_M+H113p0346_1p39",
+            1: "0001_cytosine_unlabeled_positive_M+H112p0505_4p83",
+            2: "0002_sucrose_unlabeled_positive_M+H343p1235_13p45",
+        },
+        "20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_FPS_MS1_0_QC_Pre_Rg70to1050-CE102040--QC_Run6.h5": {
+            0: 1.879153,
+            1: 4.836015,
+            2: np.NaN,
+        },
+        "20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_POS_MSMS_0_QC_Pre_Rg70to1050-CE102040--QC_Run7.h5": {
+            0: 1.889282,
+            1: 4.846456,
+            2: np.NaN,
+        },
+        "20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_FPS_MS1_0_QC_Post_Rg70to1050-CE102040--QC_Run307.h5": {
+            0: 1.878133,
+            1: 4.910717,
+            2: 13.34303,
+        },
+        "20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_POS_MSMS_0_QC_Post_Rg70to1050-CE102040--QC_Run308.h5": {
+            0: 1.892506,
+            1: 4.921309,
+            2: 13.31463,
+        },
+        "mean": {0: 1.884768, 1: 4.878624, 2: 13.32883},
+        "median": {0: 1.884217, 1: 4.878586, 2: 13.32883},
+        "min": {0: 1.878133, 1: 4.836015, 2: 13.31463},
+        "max": {0: 1.892506, 1: 4.921309, 2: 13.34303},
+        "standard deviation": {0: 0.007206614, 1: 0.04359777, 2: 0.02008078},
+        "standard error": {0: 0.003603307, 1: 0.02179888, 2: 0.01419926},
+        "#NaNs": {0: 0, 1: 0, 2: 0},
+    }
 
     command = """papermill -k papermill \
                         -p experiment 20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583 \
