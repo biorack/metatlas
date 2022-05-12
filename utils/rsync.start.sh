e3108705154fe534d536e19648ad30492364342f
Will Holtz <wholtz@gmail.com>
2022-05-12 12:38:07 -0700

    fix minor rsyncd script bugs

--- utils/rsync.start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-umask 022
-machine="$(uname -n)"
-sg msdata "rsync --daemon --config /global/common/software/m2650/metatlas-repo/utils/rsyncd.${machine}.nersc.gov.conf --port=39653 &"
