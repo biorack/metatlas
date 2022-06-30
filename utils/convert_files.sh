98d735d21e4e4c4b272d5d4a7247587297a99345
Will Holtz <wholtz@gmail.com>
2022-06-30 11:15:48 -0700

    support raw to h5 file conversion

---
+++ utils/convert_files.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ "$#" -ne 1 ] || { [ "$1" != "jgi" ] && [ "$1" != "egsb" ]; }; then
+    >&2 echo "Usage $0: search_directory"
+    >&2 echo "    where search_directory is one of 'jgi' or 'egsb'"
+    exit 128
+fi
+
+function finish {
+  module unload parallel
+}
+trap finish EXIT
+
+module load parallel
+
+base_dir="/global/cfs/cdirs/metatlas/raw_data/$1"
+
+log="/global/cfs/cdirs/m2650/file_converter_logs/${1}.log"
+
+converter='/global/common/software/m2650/metatlas-repo/utils/raw_to_h5.sh'
+
+find "$base_dir" -mindepth 2 -maxdepth 2 -type f \( -name '*.raw' -o -name '*.h5' \) | \
+  sed 's%.h5$%.raw%' | \
+  sort | \
+  uniq -u | \
+  sed "s%^%${converter} %" | \
+  parallel  >> "$log" 2>&1
---
+++ utils/raw_to_h5.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+raw_image='quay.io/biocontainers/thermorawfileparser:1.3.4--ha8f3691_0'
+
+if [ "$#" -ne 1 ]; then
+    >&2 echo "Usage $0: raw_ms_file"
+    exit 128
+fi
+
+raw_file="$(realpath "$1")"
+
+# ThermoRawFileParser.sh should return non-zero exit code on error, but it doesn't
+# https://github.com/compomics/ThermoRawFileParser/issues/140
+# But the mzML to h5 conversion will fail nicely if the mzML files does not exist
+shifter "--image=${raw_image}" ThermoRawFileParser.sh \
+	"-i=${raw_file}" "-o=$(dirname "$raw_file")" -f=1 2>&1 | \
+	ts "%Y-%m-%d %H:%M:%.S"
+
+mzml_file="${raw_file%.raw}.mzML"
+
+program="\
+from metatlas.io.file_converter import mzml_to_h5_and_add_to_db
+mzml_to_h5_and_add_to_db('${mzml_file}')"
+
+shifter "--env=PYTHONPATH=/src" "--image=doejgi/metatlas_shifter:latest" \
+        python -c "$program"
