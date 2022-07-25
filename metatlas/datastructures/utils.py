115534ae6f04bbd1f03dc80eec05c77b0c2e9631
Will Holtz <wholtz@gmail.com>
2022-07-25 14:59:20 -0700

    in config file, add unique_id field to atlas

--- metatlas/datastructures/utils.py
+++ metatlas/datastructures/utils.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from typing import NewType
+from typing import NewType, Optional
 
 from metatlas.datastructures import metatlas_objects as metob
 
@@ -12,18 +12,22 @@ AtlasName = NewType("AtlasName", str)
 Username = NewType("Username", str)
 
 
-def get_atlas(name: AtlasName, username: Username) -> metob.Atlas:
-    """Load atlas from database"""
-    atlases = metob.retrieve("Atlas", name=name, username=username)
+def get_atlas(
+    name: Optional[AtlasName] = None, username: Optional[Username] = None, unique_id: Optional[str] = None
+) -> metob.Atlas:
+    """Loads atlas from database, throws error if multiple atlases match query"""
+    args = dict(name=name, username=username, unique_id=unique_id)
+    args_no_none = {k: v for k, v in args.items() if v is not None}
+    atlases = metob.retrieve("Atlas", **args_no_none)
     try:
         if len(atlases) == 0:
-            raise ValueError(f"Database does not contain an atlas {name} owned by {username}.")
+            raise ValueError(f"Database does not contain an atlas with {args_no_none}.")
     except ValueError as err:
         logger.exception(err)
         raise err
     try:
         if len(atlases) > 1:
-            raise ValueError(f"Database contains more than one atlas {name} owned by {username}.")
+            raise ValueError(f"Database contains more than one atlas with {args_no_none}.")
     except ValueError as err:
         logger.exception(err)
         raise err
