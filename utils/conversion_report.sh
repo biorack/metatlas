22d35ded39f33b0163d4c4fa4bf87760d8b85975
Will Holtz <wholtz@gmail.com>
2022-09-13 13:56:30 -0700

    use .progress files during conversion (#692)
    * remove absolute path to script
    * safer creation of failed files
    * report failed files without errors

--- utils/conversion_report.sh
+++ utils/conversion_report.sh
@@ -23,6 +23,9 @@ function parent_dir {
   xargs --no-run-if-empty -l basename
 }
 
+function format_runs {
+  sed 's%.failed$%.raw%' | sed "s%${base_dir}/%    %"
+}
 num_converted="$(get_filenames '*.h5' -printf '.' | wc -c)"
 # Don't count .failed files less than 5 mintues old
 # as the .failed file gets created for all raw files during
@@ -35,32 +38,47 @@ num_to_convert="$(find "$base_dir" -mindepth 2 -maxdepth 2 -type f \
   uniq -u | \
   wc -l)"
 
-printf 'File conversion report for %s data.\n\n' "$1"
+readarray -t failed < <(get_filenames '*failed' -mmin +5 | sort)
+readarray -t failed_without_error < <(grep -l -v 'ERROR' "${failed[@]}" | format_runs || true)
+num_failed_without_error="${#failed_without_error[@]}"
+
+printf 'File conversion report for %s data. Ran on %s\n\n' "$1" "$(date)"
+
 printf 'Files not yet attempted: %s\n\n' "$num_to_convert"
 printf 'In the past %s days...\n' "$days"
 printf '    successfull conversions: %s\n' "$num_converted"
-printf '    failed conversions: %s\n\n' "$num_failed"
+printf '    failed conversions: %s\n' "$num_failed"
+if [ "$num_failed_without_error" -gt 0 ]; then
+  printf '    failed conversions without error messages: %s\n' "$num_failed_without_error"
+fi
+printf '\n'
 
 [ "$num_converted" = "0" ] && [ "$num_failed" = "0" ] && exit 0
 
 converted="$(get_filenames '*.h5' | sed 's%.h5$%.raw%'| sort)"
 converted_exp="$(printf '%s\n' "$converted" | parent_dir | uniq -c)"
 if [ "$num_converted" -ne "0" ]; then
-  printf 'successfull conversions per experiment:\n%s\n\n' "$converted_exp"
+  printf 'Successful conversions per experiment:\n%s\n\n' "$converted_exp"
 fi
 
 [ "$num_failed" = "0" ] && exit 0
 
-failed="$(get_filenames '*failed' -mmin +5 | sort)"
-failed_exp="$(printf '%s\n' "$failed" | parent_dir | uniq -c)"
-formated_failed="$(printf '%s\n' "$failed" | sed 's%.failed$%.raw%' | sed "s%${base_dir}/%    %")"
+failed_exp="$(printf '%s\n' "${failed[@]}" | parent_dir | uniq -c)"
+formated_failed="$(printf '%s\n' "${failed[@]}" | format_runs)"
+
 # shellcheck disable=SC2086
-errors="$(grep 'ERROR' $failed | sed 's%^%    %' | \
+errors="$(grep 'ERROR' "${failed[@]}" | sed 's%^%    %' | \
 	  sed "s%${base_dir}/%%" | \
 	  sed -E 's%\.failed.*ERROR(,|)%.raw:\n        %' | \
-	  sed -E "s%([-: ]*|)${base_dir}/[^[:space:]].*%%" )"
+	  sed -E "s%([-: ]*|)${base_dir}/[^[:space:]].*%%" )" || true
 
-printf 'failed conversions per experiment:\n%s\n\n' "$failed_exp"
+printf 'Failed conversions per experiment:\n%s\n\n' "$failed_exp"
 printf 'Failed files:\n%s\n\n' "$formated_failed"
 
-printf 'Extracted ERROR lines from .failed files:\n%s\n' "$errors"
+if [ "$num_failed_without_error" -gt 0 ]; then
+  printf 'Failed files without error messages:\n%s\n\n' "${failed_without_error[@]}"
+fi
+
+if [ -n "$errors" ]; then
+  printf 'Extracted ERROR lines from .failed files:\n%s\n' "$errors"
+fi
--- utils/raw_to_h5.sh
+++ utils/raw_to_h5.sh
@@ -13,29 +13,54 @@ if [ "$#" -ne 1 ]; then
 fi
 
 raw_file="$(realpath "$1")"
-failure_file="${raw_file%.raw}.failed"
 mzml_file="${raw_file%.raw}.mzML"
+h5_file="${raw_file%.raw}.h5"
+progress_file="${raw_file%.raw}.progress"
+failure_file="${raw_file%.raw}.failed"
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ -f "$h5_file" ]; then
+    >&2 echo "INFO: .h5 file found for ${raw_file} - skipping conversion."
+   exit 0
+fi
+
+if [ -f "$failure_file" ]; then
+    >&2 echo "INFO: .failed file found for ${raw_file} - skipping conversion."
+   exit 0
+fi
+
+function finish {
+  if [ "$done" = "true" ]; then
+    rm "${progress_file}"
+  else
+    mv "${progress_file}" "${failure_file}"
+  fi
+}
+trap finish EXIT
+
+done="false"
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+time_stamper="${script_dir}/ts.py"
 
 shifter "--env=PYTHONPATH=/src" "--image=doejgi/metatlas_shifter:latest" \
-	"${SCRIPT_DIR}/validate_file_name.py" "$raw_file" 2>&1 | \
-	"${SCRIPT_DIR}/ts.py" | \
-	tee "${failure_file}"
+	"${script_dir}/validate_file_name.py" "$raw_file" 2>&1 | \
+	"${time_stamper}" | \
+	tee "${progress_file}"
 
 # ThermoRawFileParser.sh should return non-zero exit code on error, but it doesn't
 # https://github.com/compomics/ThermoRawFileParser/issues/140
 # But the mzML to h5 conversion will fail nicely if the mzML files does not exist
 shifter "--image=${raw_image}" ThermoRawFileParser.sh \
 	"-i=${raw_file}" "-o=$(dirname "$raw_file")" -f=1 2>&1 | \
-	sed 's%^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} INFO%INFO ThermoRawFileParser:%' | \
-	"${SCRIPT_DIR}/ts.py" | \
-	tee -a "${failure_file}"
+	sed 's%^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \([A-Z]+\)%\1, ThermoRawFileParser:%' | \
+	"${time_stamper}" | \
+	tee -a "${progress_file}"
 
 shifter "--env=PYTHONPATH=/src" "--image=doejgi/metatlas_shifter:latest" \
-	"${SCRIPT_DIR}/mzml_to_h5.py" "${mzml_file}" | \
-	"${SCRIPT_DIR}/ts.py" | \
-	tee -a "${failure_file}"
+	"${script_dir}/mzml_to_h5.py" "${mzml_file}" | \
+	"${time_stamper}" | \
+	tee -a "${progress_file}"
 
 # if we made it here, none of the commands failed and the conversion is complete
-rm "${failure_file}"
+done="true"
