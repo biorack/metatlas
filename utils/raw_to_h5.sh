a57b65b452d4d9a2582835d9de4b4a00dc11b2ae
Will Holtz <wholtz@gmail.com>
2022-10-11 22:07:49 -0700

    fix: no file locking for conversion

--- utils/raw_to_h5.sh
+++ utils/raw_to_h5.sh
@@ -12,6 +12,7 @@ if [ "$#" -ne 1 ]; then
     exit 128
 fi
 
+shifter_flags=("--clearenv" "--module=none")
 raw_file="$(realpath "$1")"
 mzml_file="${raw_file%.raw}.mzML"
 h5_file="${raw_file%.raw}.h5"
@@ -30,46 +31,49 @@ trap finish EXIT
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-time_stamper="${script_dir}/ts.py"
+function process_output {
+  "${script_dir}/ts.py" | tee -a "${progress_file}"
+}
+
+rm -rf "${progress_file}"
 
 if [ -f "$h5_file" ]; then
    printf "INFO: .h5 file found for %s - skipping conversion.\n" "$raw_file" \
-	"${time_stamper}" | \
-	tee -a "${progress_file}"
+      process_output
    exit 0
 fi
 
 if [ -f "$failure_file" ]; then
    printf "INFO: .failed file found for %s - skipping conversion.\n" "$raw_file" \
-	"${time_stamper}" | \
-	tee -a "${progress_file}"
+     process_output
    exit 0
 fi
 
-shifter "--env=PYTHONPATH=/src" "--image=doejgi/metatlas_shifter:latest" \
-	"${script_dir}/validate_file_name.py" "$raw_file" 2>&1 | \
-	"${time_stamper}" | \
-	tee "${progress_file}"
+function metatlas {
+  shifter "--image=doejgi/metatlas_shifter:latest" \
+          "--env=HDF5_USE_FILE_LOCKING=FALSE" \
+          "--env=PYTHONPATH=/src" \
+	  "${shifter_flags[@]@Q}" "$@" 2>&1 | \
+  process_output
+}
 
-shifter "--image=${raw_image}" ThermoRawFileParser.sh \
-	"-i=${raw_file}" "-o=$(dirname "$raw_file")" -f=1 2>&1 | \
-	sed 's%^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \([A-Z]+\)%\1, ThermoRawFileParser:%' | \
-	"${time_stamper}" | \
-	tee -a "${progress_file}"
+metatlas "${script_dir}/validate_file_name.py" "$raw_file"
+
+shifter "${shifter_flags[@]@Q}" \
+	"--image=${raw_image}" \
+	ThermoRawFileParser.sh "-i=${raw_file}" "-o=$(dirname "$raw_file")" -f=1 2>&1 | \
+  sed 's%^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \([A-Z]+\)%\1, ThermoRawFileParser:%' | \
+  process_output
 
 # ThermoRawFileParser.sh should return non-zero exit code on error, but it doesn't
 # https://github.com/compomics/ThermoRawFileParser/issues/140
 if [ ! -f "${mzml_file}" ] || [ ! -s "${mzml_file}" ]; then
    printf "ERROR: raw to mzml conversion failed for %s, but return code was still 0.\n" "$raw_file" \
-	"${time_stamper}" | \
-	tee -a "${progress_file}"
+     process_output
    exit 1
 fi
 
-shifter "--env=PYTHONPATH=/src" "--image=doejgi/metatlas_shifter:latest" \
-	"${script_dir}/mzml_to_h5.py" "${mzml_file}" 2>&1 | \
-	"${time_stamper}" | \
-	tee -a "${progress_file}"
+metatlas "${script_dir}/mzml_to_h5.py" "${mzml_file}"
 
 # if we made it here, none of the commands failed and the conversion is complete
 done="true"
