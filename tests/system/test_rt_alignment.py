1858a625dea2d0cdb6c32821fc32639895eae90f
Will Holtz <wholtz@gmail.com>
2022-12-16 16:50:32 -0800

    fix: indexing error in atlas alignment

--- tests/system/test_rt_alignment.py
+++ tests/system/test_rt_alignment.py
@@ -98,16 +98,16 @@ atlas = HILICz150_ANT20190824_TPL_QCv3_Unlab_POS
         str(tmp_path / "505892_OakGall_final/Test-QC/0/0/Targeted/Test-QC/QC-POS/POS_QC_Measured_RTs.csv")
     ] = {
         "Unnamed: 0": {
-            0: "0000_uracil_unlabeled_positive_M+H113p0346_1p39",
-            1: "0001_2deoxyadenosine_unlabeled_positive_M+H252p1091_2p23",
-            2: "0002_adenine_unlabeled_positive_M+H136p0618_2p56",
-            3: "0003_xanthine_unlabeled_positive_M+H153p0407_2p73",
-            4: "0004_adenosine_unlabeled_positive_M+H268p1040_3p09",
-            5: "0005_cytosine_unlabeled_positive_M+H112p0505_4p83",
-            6: "0006_sucrose_unlabeled_positive_M+H343p1235_13p45",
+            0: "0000_uracil_unlabeled_positive_M+H113p0346_1p88",
+            1: "0001_2deoxyadenosine_unlabeled_positive_M+H252p1091_2p60",
+            2: "0002_adenine_unlabeled_positive_M+H136p0618_2p87",
+            3: "0003_xanthine_unlabeled_positive_M+H153p0407_3p02",
+            4: "0004_adenosine_unlabeled_positive_M+H268p1040_3p33",
+            5: "0005_cytosine_unlabeled_positive_M+H112p0505_4p88",
+            6: "0006_sucrose_unlabeled_positive_M+H343p1235_13p33",
         },
         "20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_FPS_MS1_0_QC_Pre_Rg70to1050-CE102040--QC_Run6.h5": {
-            0: 1.879153,
+            0: 1.940126,
             1: 2.24759,
             2: 2.59303,
             3: 2.747974,
@@ -116,7 +116,7 @@ atlas = HILICz150_ANT20190824_TPL_QCv3_Unlab_POS
             6: np.NaN,
         },
         "20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_POS_MSMS_0_QC_Pre_Rg70to1050-CE102040--QC_Run7.h5": {
-            0: 1.889282,
+            0: 1.938525,
             1: 2.275779,
             2: 2.625245,
             3: 2.775257,
@@ -125,7 +125,7 @@ atlas = HILICz150_ANT20190824_TPL_QCv3_Unlab_POS
             6: np.NaN,
         },
         "20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_FPS_MS1_0_QC_Post_Rg70to1050-CE102040--QC_Run307.h5": {
-            0: 1.878133,
+            0: 1.926795,
             1: 2.322836,
             2: 2.704113,
             3: 2.766577,
@@ -134,7 +134,7 @@ atlas = HILICz150_ANT20190824_TPL_QCv3_Unlab_POS
             6: 13.34303,
         },
         "20201106_JGI-AK_PS-KM_505892_OakGall_final_QE-HF_HILICZ_USHXG01583_POS_MSMS_0_QC_Post_Rg70to1050-CE102040--QC_Run308.h5": {
-            0: 1.892506,
+            0: 1.932271,
             1: 2.311643,
             2: 2.719899,
             3: 2.788285,
@@ -142,12 +142,12 @@ atlas = HILICz150_ANT20190824_TPL_QCv3_Unlab_POS
             5: 4.921309,
             6: 13.31463,
         },
-        "mean": {0: 1.884768, 1: 2.289462, 2: 2.660572, 3: 2.769523, 4: 3.110135, 5: 4.878624, 6: 13.32883},
-        "median": {0: 1.884217, 1: 2.293711, 2: 2.664679, 3: 2.770917, 4: 3.109064, 5: 4.878586, 6: 13.32883},
-        "min": {0: 1.878133, 1: 2.24759, 2: 2.59303, 3: 2.747974, 4: 3.075452, 5: 4.836015, 6: 13.31463},
-        "max": {0: 1.892506, 1: 2.322836, 2: 2.719899, 3: 2.788285, 4: 3.146962, 5: 4.921309, 6: 13.34303},
+        "mean": {0: 1.934429, 1: 2.289462, 2: 2.660572, 3: 2.769523, 4: 3.110135, 5: 4.878624, 6: 13.32883},
+        "median": {0: 1.935398, 1: 2.293711, 2: 2.664679, 3: 2.770917, 4: 3.109064, 5: 4.878586, 6: 13.32883},
+        "min": {0: 1.926795, 1: 2.24759, 2: 2.59303, 3: 2.747974, 4: 3.075452, 5: 4.836015, 6: 13.31463},
+        "max": {0: 1.940126, 1: 2.322836, 2: 2.719899, 3: 2.788285, 4: 3.146962, 5: 4.921309, 6: 13.34303},
         "standard deviation": {
-            0: 0.007206614,
+            0: 0.006114746,
             1: 0.03438155,
             2: 0.06117022,
             3: 0.01691088,
@@ -156,7 +156,7 @@ atlas = HILICz150_ANT20190824_TPL_QCv3_Unlab_POS
             6: 0.02008078,
         },
         "standard error": {
-            0: 0.003603307,
+            0: 0.003057373,
             1: 0.01719078,
             2: 0.03058511,
             3: 0.008455441,
@@ -179,6 +179,6 @@ atlas = HILICz150_ANT20190824_TPL_QCv3_Unlab_POS
                         /out/Remove-done.ipynb
     """
     utils.exec_docker(image, command, tmp_path, utils.PAPERMILL_ENV)
-    assert utils.num_files_in(tmp_path) == 22
+    assert utils.num_files_in(tmp_path) == 23
     utils.assert_files_match(expected)
     utils.assert_dfs_match(expected_df)
