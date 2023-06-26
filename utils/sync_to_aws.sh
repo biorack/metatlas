6fc6681960fa3e31981779871b2d2a64a004d028
Will Holtz <wholtz@gmail.com>
2023-06-26 16:58:30 -0600

    feat: add sync_to_aws.sh for cp to glacier

---
+++ utils/sync_to_aws.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+usage() {
+  >&2 echo "ERROR: Usage: $0 jgi|egsb"
+  exit 128
+}
+
+[ "$#" -ne "1" ] && usage
+! { [ "$1" = "jgi" ] || [ "$1" = "egsb" ]; } && usage
+
+nersc_raw_data='/global/cfs/cdirs/metatlas/raw_data'
+s3_raw_data='s3://northen-archive/raw_data'
+
+log="/global/cfs/cdirs/m2650/aws_s3_sync_log/${1}.log"
+
+aws s3 sync --storage-class GLACIER \
+            --color off \
+            --no-progress \
+            --exclude '*' \
+	    --include '*.raw' \
+	    --include '*.cmbx' \
+	    --exclude '*/other/*' \
+            "${nersc_raw_data}/${1}" \
+	    "${s3_raw_data}/${1}" \
+	    2>&1 \
+| ts \
+| tee -a "${log}"
