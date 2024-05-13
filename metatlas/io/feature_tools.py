f2afaa31d33b0089cc922258f2aca7a2b33cb4a3
Brandon Kieft <bkieft@lbl.gov>
2024-05-13 15:22:42 -0700

    Modify M1 summary centroid calculation

--- metatlas/io/feature_tools.py
+++ metatlas/io/feature_tools.py
@@ -314,10 +314,11 @@ def calculate_ms1_summary(df, feature_filter=True):
 
         summary['label'].append(label_group)
         summary['num_datapoints'].append(label_data['i'].count())
-        summary['peak_area'].append(label_data['i'].sum())
+        sum_intensity = label_data['i'].sum()
+        summary['peak_area'].append(sum_intensity)
         idx = label_data['i'].idxmax()
         summary['peak_height'].append(label_data.loc[idx,'i'])
-        summary['mz_centroid'].append(sum(label_data['i']*label_data['mz'])/summary['peak_area'])
+        summary['mz_centroid'].append(sum(label_data['i']*label_data['mz'])/sum_intensity)
         summary['rt_peak'].append(label_data.loc[idx,'rt'])
 
     return pd.DataFrame(summary)
