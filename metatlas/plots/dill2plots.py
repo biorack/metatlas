e74e8aafb8cd6a680d2e4df24d31b75766f8511d
Brandon Kieft <bkieft@lbl.gov>
2024-07-30 09:51:38 -0700

    Jaccard similarity calculator now takes frag_mz_tolerance as param instead of rounding

--- metatlas/plots/dill2plots.py
+++ metatlas/plots/dill2plots.py
@@ -2392,7 +2392,7 @@ def get_msms_hits(metatlas_dataset: MetatlasDataset, extra_time: bool | float = 
         return msms_hits
     else:
         msms_hits['msms_frag_ratio'] = msms_hits.apply(sp.calc_data_to_ref_frag_ratio, axis=1)
-        msms_hits['msms_frag_jaccard'] = msms_hits.apply(sp.calc_jaccard_of_spectra, axis=1)
+        msms_hits['msms_frag_jaccard'] = msms_hits.apply(sp.calc_jaccard_of_spectra, axis=1, **{'frag_mz_tolerance': frag_mz_tolerance})
         msms_hits = msms_hits[msms_hits_cols].set_index(['database', 'id', 'file_name', 'msms_scan'])
 
     return msms_hits
