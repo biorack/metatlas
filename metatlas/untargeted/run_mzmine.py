1703ebd2f907584c9a32b8334d955e45ebb2b18e
bkieft-usa <bkieft@lbl.gov>
2024-08-02 15:08:37 -0700

    Add new warnings and guard rails for edge cases and log

--- metatlas/untargeted/run_mzmine.py
+++ metatlas/untargeted/run_mzmine.py
@@ -9,31 +9,27 @@ import subprocess
 mzm.kickoff()
 
 ##### Find new projects in raw data that aren't in LIMS
-print('\nFinding projects in raw data that are not already in the LIMS untargeted tasks table')
-new_folders = mzm.update_new_untargeted_tasks()
+print('Finding projects in raw data that are not already in the LIMS untargeted tasks table')
+new_projects = mzm.update_new_untargeted_tasks()
 
-if len(new_folders['parent_dir']) == 0:
-    print('\nNo new projects found in raw data. Exiting script.')
+##### Add some guardrails for number of new projects that are identified
+if len(new_projects['parent_dir']) == 0:
+    print('No new projects found in raw data that are not already in untargeted tasks. Exiting script.')
+    sys.exit(0)
+if len(new_projects['parent_dir']) > 20:
+    print('There are more than 20 new projects in raw data that are not in untargeted tasks, please check if this is accurate. Exiting script.')
     sys.exit(0)
 
-##### Remove any DNA SIP jobs from untargeted tasks in LIMS
-print('\nRemoving any DNA SIP jobs from untargeted tasks in LIMS...')
-df = mzm.get_table_from_lims('untargeted_tasks',columns=['Key','parent_dir','mzmine_pos_status','mzmine_neg_status','fbmn_pos_status','fbmn_neg_status'])
-df = df[(df['parent_dir'].str.contains('DNA-SIP')) & (df['parent_dir'].str.contains('_RM'))]
-cols = [c for c in df.columns if c.endswith('status')]
-df[cols] = '12 not relevant'
-mzm.update_table_in_lims(df,'untargeted_tasks',method='update')
-
-##### Check status of mzmine jobs
-print('\nChecking and updating status of initialized or running mzmine jobs...')
+##### Check status of mzmine jobs and update LIMS untargeted tasks table
+print('Checking and updating status of initialized or running mzmine jobs...')
 mzm.update_mzmine_status_in_untargeted_tasks(polarity='positive',polarity_short='pos')
 mzm.update_mzmine_status_in_untargeted_tasks(polarity='negative',polarity_short='neg')
 
 ##### Submit new mzmine jobs
-print('\nSubmitting new mzmine jobs that are pending intitialization...\n')
+print('Submitting new mzmine jobs that are intitialized...')
 mzm.submit_mzmine_jobs(polarity='positive',polarity_short='pos')
 mzm.submit_mzmine_jobs(polarity='negative',polarity_short='neg')
 
 ##### End
-print("\nMzmine submission script complete. Monitor jobs with sqs:\n")
+print("Mzmine submission script complete. Monitor jobs with sqs:")
 subprocess.run(['sqs'], check=True)
