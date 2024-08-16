bd29718843aa9dfde232d685ee606be943cc69c2
bkieft-usa <bkieft@lbl.gov>
2024-08-16 09:34:47 -0700

    Make skipping steps an available flag

--- metatlas/untargeted/run_untargeted_pipeline.py
+++ metatlas/untargeted/run_untargeted_pipeline.py
@@ -7,30 +7,14 @@ sys.path.insert(0,'/global/homes/b/bkieft/metatlas/')
 from metatlas.untargeted import tools as mzm
 
 ##### Parse command line arguments
-parser = argparse.ArgumentParser(description='Run untargeted pipeline.')
-parser.add_argument('--download_dir', type=str, default='/global/cfs/cdirs/metatlas/projects/untargeted_outputs', help='Path to the download folder')
-parser.add_argument('--output_dir', type=str, default='/global/cfs/cdirs/metatlas/projects/untargeted_tasks', help='Path to the output directory')
-parser.add_argument('--raw_data_dir', type=str, default='/global/cfs/cdirs/metatlas/raw_data', help='Path to the raw data directory')
-parser.add_argument('--update_lims', type=bool, default=True, help='Update LIMS with new untargeted tasks')
-parser.add_argument('--validate_names', type=bool, default=True, help='Validate filenames and project names')
-parser.add_argument('--overwrite_zip',type=bool, default=False, help='Overwrite existing zip files in download folder')
-parser.add_argument('--overwrite_drive', type=bool, default=False, help='Overwrite existing zip files on google drive')
-parser.add_argument('--overwrite_fbmn', type=bool, default=False, help='Overwrite existing fbmn results files that are already in the output directory')
-parser.add_argument('--gdrive_upload', type=bool, default=True, help='Upload to google drive')
-parser.add_argument('--min_features', type=int, default=0, help='Set minimum number of MZmine features for a project polarity to be zipped')
-parser.add_argument('--add_gnps2_documentation', type=bool, default=True, help='File name of the GNPS2 documentation to add to project zips')
-parser.add_argument('--gnps2_doc_name', type=str, default='Untargeted_metabolomics_GNPS2_Guide.docx', help='File name of the GNPS2 documentation to add to project zips')
-parser.add_argument('--log_file', type=str, default='/global/homes/b/bkieft/metatlas/metatlas/untargeted/untargeted_pipeline_log.txt', help='Log file name with full path')
-parser.add_argument('--log_level', type=str, default='INFO', help='Logger level. One of [DEBUG, INFO, WARNING, ERROR, or CRITICAL]')
-parser.add_argument('--log_format', type=str, default='%(asctime)s - %(levelname)s - %(message)s', help='Logger format')
-parser.add_argument('--direct_input', type=str, default=None, help='Input project names from command line as a comma-separated list')
-parser.add_argument('--background_designator', type=str, default="TxCtrl,ExCtrl", help='Input control/background sample names from command line as a comma-separated list')
-
 def main():
 
     ##### Set arguments to pass to the pipeline steps and run some checks
+    parser = argparse.ArgumentParser(description='Run untargeted pipeline.')
+    add_arguments(parser)
     args = parser.parse_args()
     check_args(args)
+    step_bools = check_skipped_steps(args)
 
     ##### Configure logging
     mzm.call_logger(log_filename=args.log_file, log_level=args.log_level, log_format=args.log_format)
@@ -38,34 +22,55 @@ def main():
     ##### Kick off the script
     mzm.start_script(script="run_untargeted_pipeline.py")
 
-    ##### Run the pipeline steps
-    logging.info('Step 1/7: Syncing LIMS and NERSC to identify new projects with raw data that are not yet in the untargeted task list...')
+    ##### Step 1/7: Syncing LIMS and NERSC to identify new projects with raw data that are not yet in the untargeted task list
     new_projects = mzm.update_new_untargeted_tasks(update_lims=args.update_lims, validate_names=args.validate_names, \
                                                    output_dir=args.output_dir, raw_data_dir=args.raw_data_dir,
-                                                   background_designator=args.background_designator)
+                                                   background_designator=args.background_designator,skip_sync=step_bools[0])
+    ##### Step 2/7: Checking and updating status of MZmine jobs in LIMS
+    mzm.update_mzmine_status_in_untargeted_tasks(direct_input=args.direct_input,background_designator=args.background_designator, \
+                                                 skip_mzmine_status=step_bools[1])
 
-    logging.info('Step 2/7: Checking and updating status of MZmine jobs in LIMS...')
-    mzm.update_mzmine_status_in_untargeted_tasks(direct_input=args.direct_input,background_designator=args.background_designator)
+    ##### Step 3/7: Submitting new MZmine jobs that are "initialized"
+    mzm.submit_mzmine_jobs(new_projects=new_projects, direct_input=args.direct_input,skip_mzmine_submit=step_bools[2])
 
-    logging.info('Step 3/7: Submitting new MZmine jobs that are "initialized"...')
-    mzm.submit_mzmine_jobs(new_projects=new_projects, direct_input=args.direct_input)
+    ##### Step 4/7: Checking and updating status of FBMN jobs in LIMS
+    mzm.update_fbmn_status_in_untargeted_tasks(direct_input=args.direct_input,skip_fbmn_status=step_bools[3])
 
-    logging.info('Step 4/7: Checking and updating status of FBMN jobs in LIMS...')
-    mzm.update_fbmn_status_in_untargeted_tasks(direct_input=args.direct_input)
+    ##### Step 5/7: Submitting new FBMN jobs to GNPS2
+    mzm.submit_fbmn_jobs(output_dir=args.output_dir, overwrite_fbmn=args.overwrite_fbmn, validate_names=args.validate_names, \
+                         direct_input=args.direct_input, skip_fbmn_submit=step_bools[4])
 
-    logging.info('Step 5/7: Submitting new FBMN jobs to GNPS2...')
-    mzm.submit_fbmn_jobs(output_dir=args.output_dir, overwrite_fbmn=args.overwrite_fbmn, validate_names=args.validate_names,direct_input=args.direct_input)
+    ##### Step 6/7: Checking for completed FBMN jobs and downloading results
+    mzm.download_fbmn_results(output_dir=args.output_dir, overwrite_fbmn=args.overwrite_fbmn,direct_input=args.direct_input, \
+                              skip_fbmn_download=step_bools[5])
 
-    logging.info('Step 6/7: Checking for completed FBMN jobs and downloading results...')
-    mzm.download_fbmn_results(output_dir=args.output_dir, overwrite_fbmn=args.overwrite_fbmn,direct_input=args.direct_input)
-
-    logging.info("Step 7/7: Zipping up and (optionally) uploading output folders to gdrive...")
+    ##### Step 7/7: Zipping up and (optionally) uploading output folders to gdrive
     mzm.zip_and_upload_untargeted_results(download_folder=args.download_dir,output_dir=args.output_dir,upload=args.gdrive_upload,overwrite_zip=args.overwrite_zip, \
-                                          overwrite_drive=args.overwrite_drive, min_features_admissible=args.min_features, \
+                                          overwrite_drive=args.overwrite_drive, min_features_admissible=args.min_features, skip_zip_upload=step_bools[6], \
                                           add_documentation=args.add_gnps2_documentation,doc_name=args.gnps2_doc_name,direct_input=args.direct_input)
 
     ##### End the script
     mzm.end_script(script="run_untargeted_pipeline.py")
+
+def add_arguments(parser):
+    parser.add_argument('--download_dir', type=str, default='/global/cfs/cdirs/metatlas/projects/untargeted_outputs', help='Path to the download folder')
+    parser.add_argument('--output_dir', type=str, default='/global/cfs/cdirs/metatlas/projects/untargeted_tasks', help='Path to the output directory')
+    parser.add_argument('--raw_data_dir', type=str, default='/global/cfs/cdirs/metatlas/raw_data', help='Path to the raw data directory')
+    parser.add_argument('--update_lims', type=bool, default=True, help='Update LIMS with new untargeted tasks')
+    parser.add_argument('--validate_names', type=bool, default=True, help='Validate filenames and project names')
+    parser.add_argument('--overwrite_zip',type=bool, default=False, help='Overwrite existing zip files in download folder')
+    parser.add_argument('--overwrite_drive', type=bool, default=False, help='Overwrite existing zip files on google drive')
+    parser.add_argument('--overwrite_fbmn', type=bool, default=False, help='Overwrite existing fbmn results files that are already in the output directory')
+    parser.add_argument('--gdrive_upload', type=bool, default=True, help='Upload to google drive')
+    parser.add_argument('--min_features', type=int, default=0, help='Set minimum number of MZmine features for a project polarity to be zipped')
+    parser.add_argument('--add_gnps2_documentation', type=bool, default=True, help='File name of the GNPS2 documentation to add to project zips')
+    parser.add_argument('--gnps2_doc_name', type=str, default='Untargeted_metabolomics_GNPS2_Guide.docx', help='File name of the GNPS2 documentation to add to project zips')
+    parser.add_argument('--log_file', type=str, default='/global/homes/b/bkieft/metatlas/metatlas/untargeted/untargeted_pipeline_log.txt', help='Log file name with full path')
+    parser.add_argument('--log_level', type=str, default='INFO', help='Logger level. One of [DEBUG, INFO, WARNING, ERROR, or CRITICAL]')
+    parser.add_argument('--log_format', type=str, default='%(asctime)s - %(levelname)s - %(message)s', help='Logger format')
+    parser.add_argument('--direct_input', type=str, default=None, help='Input project names from command line as a CSV list')
+    parser.add_argument('--background_designator', type=str, default="TxCtrl,ExCtrl", help='Input control/background sample names from command line as a CSV list')
+    parser.add_argument('--skip_steps', type=str, default=None, help='Skip pipeline step(s) with --direct_input. CSV list with elements [Sync,MZmine_Status,MZmine_Submit,FBMN_Status,FBMN_Submit,FBMN_Download,Zip_and_Upload]')
 
 def check_args(args):
     ##### Check if the input arguments are valid
@@ -73,6 +78,11 @@ def check_args(args):
         args.direct_input = args.direct_input.split(',')
     if args.background_designator:
         args.background_designator = args.background_designator.split(',')
+    if args.skip_steps:
+        args.skip_steps = args.skip_steps.split(',')
+    if args.skip_steps is not None and args.direct_input is None:
+        logging.error('Incompatible flags. Must provide direct input if you want to skip steps.')
+        sys.exit(1)
     if args.overwrite_drive is True and args.gdrive_upload is False:
         logging.error('Incompatible flags. Cannot overwrite google drive if not uploading to google drive.')
         sys.exit(1)
@@ -89,5 +99,27 @@ def check_args(args):
         logging.error('Raw data directory does not exist. Please check flag.')
         sys.exit(1)
 
+def check_skipped_steps(args):
+    ##### Check for steps to skip during direct input
+    if args.skip_steps is not None and args.direct_input is not None:
+        step_skip_bool_list = [False] * 7
+        if 'Sync' in args.skip_steps:
+            step_skip_bool_list[0] = True
+        if 'MZmine_Status' in args.skip_steps:
+            step_skip_bool_list[1] = True
+        if 'MZmine_Submit' in args.skip_steps:
+            step_skip_bool_list[2] = True
+        if 'FBMN_Status' in args.skip_steps:
+            step_skip_bool_list[3] = True
+        if 'FBMN_Submit' in args.skip_steps:
+            step_skip_bool_list[4] = True
+        if 'FBMN_Download' in args.skip_steps:
+            step_skip_bool_list[5] = True
+        if 'Zip_and_Upload' in args.skip_steps:
+            step_skip_bool_list[6] = True
+    else:
+        step_skip_bool_list = [False] * 7
+    return step_skip_bool_list
+    
 if __name__ == "__main__":
     main()
