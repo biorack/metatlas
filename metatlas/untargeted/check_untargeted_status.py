dce5833cdd0f2b78dda3ffdd8e31beb2fc093474
bkieft-usa <bkieft@lbl.gov>
2024-08-26 10:30:14 -0700

    Order LIMS dataframe by date before passing to project email report

--- metatlas/untargeted/check_untargeted_status.py
+++ metatlas/untargeted/check_untargeted_status.py
@@ -8,6 +8,7 @@ def add_arguments(parser):
     parser.add_argument('--direct_input', type=str, default=None, help='Input project names from command line as a CSV list')
     parser.add_argument('--background_designator', type=str, default="ExCtrl", help='Input control/background sample names from command line as a CSV list')
     parser.add_argument('--print_recent', type=int, default=50, help='Print status for the most recent N projects')
+    parser.add_argument('--update_lims_table', type=bool, default=False, help='Update LIMS table before getting job status')
 
 def check_args(args):
     ##### Check if the input arguments are valid
@@ -19,25 +20,19 @@ def check_args(args):
         args.print_recent = 100000 # Print all projects if direct_input is specified
 
 def main():
-    ##### Kick off the script
-    start_message = mzm.start_script(script="check_untargeted_status.py")
-    print(start_message)
 
     ##### Set arguments to pass to the pipeline steps and run some checks
     parser = argparse.ArgumentParser(description='Run untargeted pipeline.')
     add_arguments(parser)
     args = parser.parse_args()
 
-    print('Step 1/3: Checking and updating status of MZmine jobs in LIMS...')
-    mzm.update_mzmine_status_in_untargeted_tasks(background_designator=args.background_designator)
-    print('Step 2/3: Checking and updating status of FBMN jobs in LIMS...')
-    mzm.update_fbmn_status_in_untargeted_tasks()
-    print('Step 3/3: Getting job status for untargeted projects...')
+    if args.update_lims_table is True:
+        print('Checking and updating status of MZmine jobs in LIMS...')
+        mzm.update_mzmine_status_in_untargeted_tasks(background_designator=args.background_designator)
+        print('Checking and updating status of FBMN jobs in LIMS...')
+        mzm.update_fbmn_status_in_untargeted_tasks()
+    print('Getting job status for untargeted projects...')
     mzm.get_untargeted_status(direct_input=args.direct_input,print_recent=args.print_recent)
-
-    ##### End the script
-    end_message = mzm.end_script(script="check_untargeted_status.py")
-    print(end_message)
 
 if __name__ == "__main__":
     main()
