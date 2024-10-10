ce97a78ea8fa373f81c744b9af4a63882e860ece
Brandon Kieft <162646417+bkieft-usa@users.noreply.github.com>
2024-10-10 13:58:57 -0700

    Fix default behavior of flags passed to untargeted pipeline

--- metatlas/untargeted/run_untargeted_pipeline.py
+++ metatlas/untargeted/run_untargeted_pipeline.py
@@ -27,7 +27,7 @@ def main():
     logging.info(f'Arguments used: {args}')
 
     ##### Step 1/7: Syncing LIMS and NERSC to identify new projects with raw data that are not yet in the untargeted task list
-    new_projects = mzm.update_new_untargeted_tasks(update_lims=args.update_lims, validate_names=args.validate_names, direct_input=args.direct_input,\
+    new_projects = mzm.update_new_untargeted_tasks(validate_names=args.validate_names, direct_input=args.direct_input,\
                                                    output_dir=args.output_dir, raw_data_dir=args.raw_data_dir, raw_data_subdir=args.raw_data_subdir, \
                                                    background_designator=args.background_designator,skip_sync=step_bools[0])
     
@@ -46,7 +46,7 @@ def main():
 
     ##### Step 5/7: Submitting new FBMN jobs to GNPS2
     mzm.submit_fbmn_jobs(output_dir=args.output_dir, overwrite_fbmn=args.overwrite_fbmn, direct_input=args.direct_input, skip_fbmn_submit=step_bools[4], \
-                        mirror_raw_data=args.mirror_raw_data, mirror_mzmine_results=args.mirror_mzmine_results, raw_data_dir=args.raw_data_dir,raw_data_subdir=args.raw_data_subdir)
+                        skip_mirror_raw_data=args.skip_mirror_raw_data, skip_mirror_mzmine_results=args.skip_mirror_mzmine_results, raw_data_dir=args.raw_data_dir,raw_data_subdir=args.raw_data_subdir)
 
     ##### Step 6/7: Checking for completed FBMN jobs and downloading results
     mzm.download_fbmn_results(output_dir=args.output_dir, overwrite_fbmn=args.overwrite_fbmn,direct_input=args.direct_input, \
@@ -66,34 +66,39 @@ def main():
 def add_arguments(parser):
     ## Multiple functions
     parser.add_argument('--output_dir', type=str, default='/global/cfs/cdirs/metatlas/projects/untargeted_tasks', help='Path to the output directory')
-    parser.add_argument('--overwrite_fbmn', type=bool, default=False, help='Overwrite existing fbmn results files that are already in the output directory')
     parser.add_argument('--direct_input', type=str, default=None, help='Input project names from command line as a CSV list')
     parser.add_argument('--background_designator', type=str, default="ExCtrl", help='Input control/background sample names from command line as a CSV list')
     parser.add_argument('--skip_steps', type=str, default=None, help='Skip pipeline step(s) with --direct_input. CSV list with elements [Sync,MZmine_Status,MZmine_Submit,FBMN_Status,FBMN_Submit,FBMN_Download,Zip_and_Upload]')
     parser.add_argument('--raw_data_dir', type=str, default='/global/cfs/cdirs/metatlas/raw_data', help='Path to the raw data directory')
-    parser.add_argument('--raw_data_subdir', type=str, default=None, help='Name of the raw_data subdirectory (e.g., jgi, egsb) to use for tasks that require raw data files. If not given, will try to infer from the project name.')
+    parser.add_argument('--raw_data_subdir', type=str, default=None, help='Name of the raw_data subdirectory (e.g., jgi, egsb) to use for tasks that require raw data files. If not given, will try to infer from the second field of the project name.')
+    parser.add_argument('--overwrite_fbmn', action='store_true', help='Overwrite existing fbmn results files that are already in the output directory')
     ## Step 1 only
-    parser.add_argument('--update_lims', type=bool, default=True, help='Update LIMS with new untargeted tasks')
-    parser.add_argument('--validate_names', type=bool, default=False, help='Validate filenames and project names')
+    parser.add_argument('--validate_names', action='store_true', help='Validate filenames and project names')
     ## Step 2 only
     parser.add_argument('--background_ratio', type=float, default=5, help='Ratio of background to sample intensity for filtering features')
     parser.add_argument('--zero_value', type=float, default=(2/3), help='Proportion of the lowest intensity value from the experiment to use as replacement zero value')
     parser.add_argument('--polar_solvent_front', type=float, default=0.8, help='Retention time to use as HILIC solvent front (mins) for filtering features')
     parser.add_argument('--nonpolar_solvent_front', type=float, default=0.5, help='Retention time to use as C18/LIPID solvent front (mins) for filtering features')
     ## Step 3 only
-    parser.add_argument('--overwrite_mzmine', type=bool, default=False, help='Overwrite existing mzmine results files that are already in the output directory')
+    parser.add_argument('--overwrite_mzmine', action='store_true', help='Overwrite existing mzmine results files that are already in the output directory')
     ## Step 5 only
-    parser.add_argument('--mirror_raw_data', type=bool, default=True, help='Mirror raw data files to GNPS2')
-    parser.add_argument('--mirror_mzmine_results', type=bool, default=True, help='Mirror mzmine results files to GNPS2')
+    parser.add_argument('--skip_mirror_raw_data', action='store_true', help='Skip the default mirror of raw data files to GNPS2')
+    parser.add_argument('--skip_mirror_mzmine_results', action='store_true', help='Skip the default mirror of mzmine results files to GNPS2')
     ## Step 7 only
     parser.add_argument('--download_dir', type=str, default='/global/cfs/cdirs/metatlas/projects/untargeted_outputs', help='Path to the download folder')
-    parser.add_argument('--overwrite_zip',type=bool, default=False, help='Overwrite existing zip files in download folder')
-    parser.add_argument('--overwrite_drive', type=bool, default=False, help='Overwrite existing zip files on google drive')
-    parser.add_argument('--gdrive_upload', type=bool, default=True, help='Upload to google drive')
     parser.add_argument('--min_features', type=int, default=0, help='Set minimum number of MZmine features for a project polarity to be zipped')
-    parser.add_argument('--add_gnps2_documentation', type=bool, default=True, help='File name of the GNPS2 documentation to add to project zips')
     parser.add_argument('--gnps2_doc_name', type=str, default='Untargeted_metabolomics_GNPS2_Guide.docx', help='File name of the GNPS2 documentation to add to project zips')
-    parser.add_argument('--abridged_filenames', type=bool, default=True, help='Use abridged filenames in the zipped folders. Can be set to False for non-conforming project names')
+    parser.add_argument('--overwrite_zip', action='store_true', help='Overwrite existing zip files in download folder')
+    parser.add_argument('--overwrite_drive', action='store_true', help='Overwrite existing zip files on google drive')
+    parser.add_argument('--gdrive_upload', action='store_true', help='Upload to google drive')
+    parser.add_argument('--no_gdrive_upload', action='store_false', dest='gdrive_upload', help='Do not upload to google drive')
+    parser.add_argument('--add_gnps2_documentation', action='store_true', help='Add GNPS2 documentation to project zips')
+    parser.add_argument('--no_add_gnps2_documentation', action='store_false', dest='add_gnps2_documentation', help='Do not add GNPS2 documentation to project zips')
+    parser.add_argument('--abridged_filenames', action='store_true', help='Use abridged filenames in the zipped folders. Can be set to False for non-conforming project names')
+    parser.add_argument('--no_abridged_filenames', action='store_false', dest='abridged_filenames', help='Do not use abridged filenames in the zipped folders')
+    parser.set_defaults(gdrive_upload=True)
+    parser.set_defaults(add_gnps2_documentation=True)
+    parser.set_defaults(abridged_filenames=True)
     ## Logger/helper
     parser.add_argument('--log_file', type=str, default='/global/cfs/cdirs/m2650/untargeted_logs/untargeted_pipeline.log', help='Log file name with full path')
     parser.add_argument('--log_level', type=str, default='INFO', help='Logger level. One of [DEBUG, INFO, WARNING, ERROR, or CRITICAL]')
@@ -113,17 +118,14 @@ def check_args(args):
     if args.overwrite_drive is True and args.gdrive_upload is False:
         logging.error('Incompatible flags. Cannot overwrite google drive if not uploading to google drive.')
         sys.exit(1)
-    if args.gnps2_doc_name is None and args.add_gnps2_documentation is True:
-        logging.error('Incompatible flags. Must provide GNPS2 document name if you want to add it to the zip.')
-        sys.exit(1)
     if not os.path.exists(args.download_dir):
-        logging.error('Download directory does not exist. Please check flag.')
+        logging.error('Download directory does not exist. Please check flag and that you are at NERSC.')
         sys.exit(1)
     if not os.path.exists(args.output_dir):
-        logging.error('Output directory does not exist. Please check flag.')
+        logging.error('Output directory does not exist. Please check flag and that you are at NERSC.')
         sys.exit(1)
     if not os.path.exists(args.raw_data_dir):
-        logging.error('Raw data directory does not exist. Please check flag.')
+        logging.error('Raw data directory does not exist. Please check flag and that you are at NERSC.')
         sys.exit(1)
 
 def check_skipped_steps(args):
