c857beaf6b5f25dd98a11a875c672476db4c9591
Brandon Kieft <162646417+bkieft-usa@users.noreply.github.com>
2025-03-11 11:41:19 -0700

    Create notebook and script for renaming files to new raw directory

--- metatlas/scripts/copy_and_rename_raw_files.py
+++ metatlas/scripts/copy_and_rename_raw_files.py
@@ -1,6 +1,5 @@
-import sys
 import os
-import csv
+import pandas as pd
 import shutil
 from pathlib import Path
 import argparse
@@ -8,64 +7,85 @@ import argparse
 from metatlas.tools.validate_filenames import validate_file_name
 from metatlas.tools.logging import activate_module_logging
 
-def activate_logging():
-    logger = activate_module_logging(
-        __name__,
-        console_level="INFO",
-        console_format="{color}{levelname:8}{reset} {message}",
-        file_level="INFO",
-        filename=Path("/global/cfs/cdirs/m2650/copy_rename_logs"),
-    )
-    return logger
+class ValidationError(Exception):
+    pass
 
-def read_in_rename_table(table_file):
-    with open(table_file, mode='r') as file:
-        reader = csv.reader(file)
-        return list(reader)
-
-def check_file_name(file_name, logger):
-    valid_name = validate_file_name(file_name, minimal=True)
+def check_file_name(file_name):
+    valid_name = validate_file_name(file_name, minimal=True, print_logger=False)
     if valid_name is not True:
-        logger.critical(f"New filename {file_name} failed required validation check. Check that file name conforms.")
-        sys.exit(1)
+        raise ValidationError(f"New filename '{file_name}' failed required validation check.")
 
-def check_directory_name(directory_name, logger):
-    base_name = Path(directory_name).name
+def check_directory_name(directory_name):
+    base_name = directory_name.name
     num_fields = len(base_name.split("_"))
     if num_fields < 9:
-        logger.critical(f"New directory name {directory_name} failed required validation check. It has {num_fields} fields, but at least 9 are required.")
-        sys.exit(1)
+        raise ValidationError(f"New project name '{directory_name}' failed required validation check.")
 
-def copy_and_rename_raw_files(table, current_path, new_path, logger):
+def check_file_name_uniqueness(table):
+    if len(table.iloc[:, 1].unique()) != len(table):
+        raise ValidationError(f"New filenames provided in input naming table are not unique.")
 
-    if not os.path.exists(new_path): # Create new project directory if it doesn't exist
-        check_directory_name(new_path, logger)
-        os.makedirs(new_path)
-        shutil.chown(new_path, group='metatlas')
-        logger.info(f"Notice! Had to make new project directory: {new_path}")
+def default_logger():
+    return activate_module_logging(
+            __name__,
+            console_level="INFO",
+            console_format="{color}{levelname:8}{reset} {message}",
+            file_level="INFO",
+            filename=Path("/global/cfs/cdirs/m2650/copy_rename_logs"),
+        )
 
-    for current_name, new_name in table:
-        check_file_name(new_name, logger)
+def copy_and_rename(table_file, current_path, new_path, checks, logger=None):
+
+    logger = logger if logger is not None else default_logger()
+
+    if not os.path.exists(table_file):
+        raise ValidationError(f"Table file not found: {table_file}")
+    else:
+        rename_table = pd.read_csv(table_file, header=None, index_col=False)
+
+    if not os.path.exists(new_path):
+        if checks is True:
+            check_directory_name(Path(new_path))
+        try:
+            os.makedirs(new_path)
+            shutil.chown(new_path, group='metatlas')
+        except Exception as e:
+            logger.error(f"Failed to create new directory: {new_path}")
+            logger.error(e)
+
+    if checks is True:
+        check_file_name_uniqueness(rename_table)
+
+    for index, row in rename_table.iterrows():
+        current_name, new_name = row.iloc[0], row.iloc[1]
+        if checks is True:
+            check_file_name(Path(new_name))
         current_file = os.path.join(current_path, current_name)
         new_file = os.path.join(new_path, new_name)
         if os.path.exists(current_file):
             shutil.copy2(current_file, new_file)
             logger.info(f"Copied: {current_file} -> {new_file}")
         else:
-            logger.warning(f"File not found: {current_file}")
+            logger.error(f"File not found: {current_file}")
 
 def main():
     parser = argparse.ArgumentParser(description="Rename raw files based on a CSV table and copy to new location.")
-    parser.add_argument("table_file", help="CSV file with two columns: current_name, new_name")
-    parser.add_argument("current_path", help="Path to the current files")
-    parser.add_argument("new_path", help="Path to the new files")
+    parser.add_argument("table_file", type=str, help="CSV file with two columns: current_name, new_name")
+    parser.add_argument("current_path", type=str, help="Path to the current files")
+    parser.add_argument("new_path", type=str, help="Path to the new files")
+    parser.add_argument("checks", type=bool, help="True for checks on new names, else False")
 
     args = parser.parse_args()
 
-    logger = activate_logging()
+    logger = activate_module_logging(
+            __name__,
+            console_level="INFO",
+            console_format="{color}{levelname:8}{reset} {message}",
+            file_level="INFO",
+            filename=Path("/global/cfs/cdirs/m2650/copy_rename_logs"),
+            )
 
-    rename_table = read_in_rename_table(args.table_file)
-    copy_and_rename_raw_files(rename_table, args.current_path, args.new_path, logger)
+    copy_and_rename(args.table_file, args.current_path, args.new_path, checks=args.checks, logger=logger)
 
 if __name__ == "__main__":
     main()
